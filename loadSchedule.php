8c89914a444d7c23c1b34987867108f6351bcb5d
Kenny Muñoz <kenny@bnka.com>
2024-02-04 16:15:35 -0500

    s

--- loadSchedule.php
+++ loadSchedule.php
@@ -14,13 +14,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['date'])) {
             $sql2 = "SELECT cs.id, cs.h_start, cs.h_end FROM Custom_Schedule cs LEFT JOIN Training t ON t.training_date = cs.t_date WHERE t.training_date = '$date' AND t.schedule_id != cs.id";
             $result2 = $conn->query($sql2);
             while ($row2 = $result2->fetch_assoc()) {
-                echo '<li><a href="">' . $row['h_start'] . '</a></li>';
+                echo '<li><a href="">' . $row2['h_start'] . '</a></li>';
             }
         } else {
             $sql2 = "SELECT ds.id, ds.h_start, ds.h_end FROM Default_Schedule ds LEFT JOIN Training t ON ds.id = t.schedule_id AND t.training_date = '$date' WHERE t.id IS NULL";
             $result2 = $conn->query($sql2);
             while ($row2 = $result2->fetch_assoc()) {
-                echo '<li><a href="">' . $row['h_start'] . '</a></li>';
+                echo '<li><a href="">' . $row2['h_start'] . '</a></li>';
             }
         }
         echo '</ul>';
