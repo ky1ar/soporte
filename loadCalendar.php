2564a608f94cc66f9377547484dc79bf5cc68dff
Kenny Muñoz <kenny@munoz.pe>
2024-02-07 16:04:37 -0500

    s

--- loadCalendar.php
+++ loadCalendar.php
@@ -14,13 +14,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['date'])) {
     $dayMonth = $selectedDate->format('j');
     $monthNumber = $selectedDate->format('n');
 
+    echo $todayMonth . ' == ' . $monthNumber . ' | '. $todayDay . ' == ' . $dayMonth;
     for ($i = 0; $i < $day; $i++) { echo '<li></li>'; }
     
     $sql = "SELECT * FROM Calendar WHERE YEAR(calendar_date) = YEAR('$date') AND MONTH(calendar_date) = MONTH('$date')";
     $result = $conn->query($sql);
     while ($row = $result->fetch_assoc()) {
         
-        echo $todayMonth . ' == ' . $monthNumber . ' | '. $todayDay . ' == ' . $dayMonth;
+        
         $dayNum = date('d', strtotime($row['calendar_date']));
         $state = $row['state'];
         
