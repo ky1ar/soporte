143dfb049bdaf834056655150d314804a88cb10b
Kenny Muñoz <kenny@munoz.pe>
2024-02-17 15:00:42 -0500

    s

--- capacitaciones.php
+++ capacitaciones.php
@@ -55,9 +55,7 @@ setlocale(LC_TIME, 'es_ES');
                     </ul>
                 </div>
                 <div class="cap-rgt" id="scheduleCalendar">
-                    <div id="loadingResponse">
-                        <img src="assets/img/fav.png" alt="Cargando..." />
-                    </div>
+                    
                     <?php
                     $today = date('d');
                     $firstDay = date('Y-m-01');
@@ -74,99 +72,106 @@ setlocale(LC_TIME, 'es_ES');
                             <div class="btn txt" id="calendarBack">Volver</div>
                         </div>
                     </div>
+                    <div id="calendarContainer">
+                        <div id="loadingResponse">
+                            <div class="flex">
+                                <img src="assets/img/fav.png" alt="Cargando..." />
+                            </div>
+                        </div>
                     
-                    <div id="calendarSelector">
-                        <ul class="cld-box box-hdr">
-                            <li>dom</li>
-                            <li>lun</li>
-                            <li>mar</li>
-                            <li>mié</li>
-                            <li>jue</li>
-                            <li>vie</li>
-                            <li>sáb</li>
-                        </ul>
-                        <ul class="cld-box" id="calendarTable">
-                            <?php
-                            for ($i = 0; $i < $firstDayNum; $i++) { echo '<li></li>'; }
-                            
-                            $sql = "SELECT * FROM Calendar WHERE YEAR(calendar_date) = YEAR('$firstDay') AND MONTH(calendar_date) = MONTH('$firstDay')";
-                            $result = $conn->query($sql);
-                            while ($row = $result->fetch_assoc()) {
-                                $dayNum = date('d', strtotime($row['calendar_date']));
-                                $state = $row['state'];
+                        <div id="calendarSelector">
+                            <ul class="cld-box box-hdr">
+                                <li>dom</li>
+                                <li>lun</li>
+                                <li>mar</li>
+                                <li>mié</li>
+                                <li>jue</li>
+                                <li>vie</li>
+                                <li>sáb</li>
+                            </ul>
+                            <ul class="cld-box" id="calendarTable">
+                                <?php
+                                for ($i = 0; $i < $firstDayNum; $i++) { echo '<li></li>'; }
                                 
-                                echo '<li' . (($today == $dayNum) ? ' class="today"' : '') . '>';
-                                if ( $dayNum <= $today || $state == 0 ) { echo '<span>'.$dayNum.'</span>'; } 
-                                else { echo '<div class="boxDay" data-day="'.$dayNum.'">'.$dayNum.'</div>'; }
-                                echo '</li>';
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                    <div id="scheduleSelector"></div>
-                    <div id="scheduleForm">
-                        <form action="post" id="scheduleSubmit" enctype="multipart/form-data">
-                            <div id="selectedSchedule"></div>
-                            <p class="selectedMessage">Completa los campos para reservar tu cita.</p>
-                            
-                            <ul>
-                                <li class="percent30">
-                                    <div class="formRow">
-                                        <label for="">Documento</label>
-                                        <input id="dniRUC" type="text" placeholder="DNI / RUC">
-                                        <input id="clientId" type="hidden">
-                                    </div>
-                                </li>
-                                <li class="percent70">
-                                    <div class="formRow">
-                                        <label for="">Cliente</label>
-                                        <input id="client" type="text" placeholder="Nombre del cliente">
-                                    </div>
-                                </li>
-                                <li class="percent60">
-                                    <div class="formRow">
-                                        <label for="">Email</label>
-                                        <input id="email" type="email" placeholder="Ingresar correo">
-                                    </div>
-                                </li>
-                                <li class="percent40">
-                                    <div class="formRow">
-                                        <label for="">Celular</label>
-                                        <input id="phone" type="tel" placeholder="Ingresar celular">
-                                    </div>
-                                </li>
-                                <div class="percent75">
-                                    <li class="percent100">
+                                $sql = "SELECT * FROM Calendar WHERE YEAR(calendar_date) = YEAR('$firstDay') AND MONTH(calendar_date) = MONTH('$firstDay')";
+                                $result = $conn->query($sql);
+                                while ($row = $result->fetch_assoc()) {
+                                    $dayNum = date('d', strtotime($row['calendar_date']));
+                                    $state = $row['state'];
+                                    
+                                    echo '<li' . (($today == $dayNum) ? ' class="today"' : '') . '>';
+                                    if ( $dayNum <= $today || $state == 0 ) { echo '<span>'.$dayNum.'</span>'; } 
+                                    else { echo '<div class="boxDay" data-day="'.$dayNum.'">'.$dayNum.'</div>'; }
+                                    echo '</li>';
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                        <div id="scheduleSelector"></div>
+                        <div id="scheduleForm">
+                            <form action="post" id="scheduleSubmit" enctype="multipart/form-data">
+                                <div id="selectedSchedule"></div>
+                                <p class="selectedMessage">Completa los campos para reservar tu cita.</p>
+                                
+                                <ul>
+                                    <li class="percent30">
                                         <div class="formRow">
-                                            <label for="">Producto</label>
-                                            <div class="formMachine">
-                                                <input id="machine" type="text" placeholder="Nombre del equipo">
-                                                <input id="machineId" type="hidden" >
-                                                <div id="suggestions"></div>
+                                            <label for="">Documento</label>
+                                            <input id="dniRUC" type="text" placeholder="DNI / RUC">
+                                            <input id="clientId" type="hidden">
+                                        </div>
+                                    </li>
+                                    <li class="percent70">
+                                        <div class="formRow">
+                                            <label for="">Cliente</label>
+                                            <input id="client" type="text" placeholder="Nombre del cliente">
+                                        </div>
+                                    </li>
+                                    <li class="percent60">
+                                        <div class="formRow">
+                                            <label for="">Email</label>
+                                            <input id="email" type="email" placeholder="Ingresar correo">
+                                        </div>
+                                    </li>
+                                    <li class="percent40">
+                                        <div class="formRow">
+                                            <label for="">Celular</label>
+                                            <input id="phone" type="tel" placeholder="Ingresar celular">
+                                        </div>
+                                    </li>
+                                    <div class="percent75">
+                                        <li class="percent100">
+                                            <div class="formRow">
+                                                <label for="">Producto</label>
+                                                <div class="formMachine">
+                                                    <input id="machine" type="text" placeholder="Nombre del equipo">
+                                                    <input id="machineId" type="hidden" >
+                                                    <div id="suggestions"></div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li class="percent100">
-                                        <div class="formRow">
-                                            <label for="">Boleta</label>
-                                            <input type="file" id="invoice" accept=".pdf,.jpg,.jpeg">
-                                        </div>
-                                    </li>
-                                </div>
-                                <li class="percent25">
-                                    <div class="formRow">
-                                        <label for="">Imagen</label>
-                                        <img id="machineImage" src="assets/img/def.webp" alt="">
+                                        </li>
+                                        <li class="percent100">
+                                            <div class="formRow">
+                                                <label for="">Boleta</label>
+                                                <input type="file" id="invoice" accept=".pdf,.jpg,.jpeg">
+                                            </div>
+                                        </li>
                                     </div>
-                                </li>
-                            </ul> 
-                            <div class="formButton">
-                                <input type="hidden" id="picked">
-                                <div id="scheduleFormMessage"></div>
-                                <button type="submit" class="formSubmit">Confirmar Reserva</button>
-                            </div>     
-                        </form>
-                    </div>
+                                    <li class="percent25">
+                                        <div class="formRow">
+                                            <label for="">Imagen</label>
+                                            <img id="machineImage" src="assets/img/def.webp" alt="">
+                                        </div>
+                                    </li>
+                                </ul> 
+                                <div class="formButton">
+                                    <input type="hidden" id="picked">
+                                    <div id="scheduleFormMessage"></div>
+                                    <button type="submit" class="formSubmit">Confirmar Reserva</button>
+                                </div>     
+                            </form>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </div>
