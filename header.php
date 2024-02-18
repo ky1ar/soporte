5afd12ed85de0f10eba953b3e6b32ab08cf7f087
Kenny Muñoz <kenny@munoz.pe>
2024-02-17 20:33:59 -0500

    s

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/ky1ar.css?v=1.93">
-<link rel="stylesheet" href="assets/css/ky1-res.css?v=1.93">
+<link rel="stylesheet" href="assets/css/ky1ar.css?v=1.94">
+<link rel="stylesheet" href="assets/css/ky1-res.css?v=1.94">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/js/ky1ar.js?v=1.93"></script>
+<script type="text/javascript" src="assets/js/ky1ar.js?v=1.94"></script>
