12580a756d9939f2c0975034fab3a7a1a033bfee
Kenny Muñoz <kenny@munoz.pe>
2024-02-19 12:41:27 -0500

    s

--- header_back.php
+++ header_back.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/ky1ar.css?v=2.010">
-<link rel="stylesheet" href="assets/css/ky1-res.css?v=2.010">
+<link rel="stylesheet" href="assets/css/ky1ar.css?v=2.011">
+<link rel="stylesheet" href="assets/css/ky1-res.css?v=2.011">
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/js/ky1ar.js?v=2.010"></script>
+<script type="text/javascript" src="assets/js/ky1ar.js?v=2.011"></script>
