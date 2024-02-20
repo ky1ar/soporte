00330e35a625c4ba936ecabb7b543b760ac74581
Kenny Muñoz <kenny@munoz.pe>
2024-02-20 16:26:05 -0500

    s

--- header_front.php
+++ header_front.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="assets/css/ky1ar-front.css?v=0.325">
-<script type="text/javascript" src="assets/js/ky1ar-training.js?v=0.325"></script>
+<link rel="stylesheet" href="assets/css/ky1ar-front.css?v=0.326">
+<script type="text/javascript" src="assets/js/ky1ar-training.js?v=0.326"></script>
