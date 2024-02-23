da0b240cf45722fb34b0cb2de30db45262f3f0bc
Kenny Muñoz <kenny@munoz.pe>
2024-02-23 12:32:24 -0500

    s

--- includes/navBar.php
+++ includes/navBar.php
@@ -2,7 +2,7 @@
     <div class="ky1-wrp">
         <a href="/"><img class="hdr-lgo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
         <ul>
-            <li><a <?php if($currentPage == 'Citas') echo 'class="active"'; ?> href="#">Consultar</a></li>
+            <li><a <?php if($currentPage == 'Citas') echo 'class="active"'; ?> href="/">Consultar</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="capacitaciones">Capacitaciones</a></li>
             <li><a <?php if($currentPage == 'Conocimiento') echo 'class="active"'; ?> href="#">Conocimiento</a></li>
             <li><a <?php if($currentPage == 'Cursos') echo 'class="active"'; ?> href="#">Cursos</a></li>
