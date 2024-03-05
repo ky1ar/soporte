f4e99cc417df02390e539d7280ca0fdeb202ba0a
Kenny Muñoz <kenny@munoz.pe>
2024-03-05 17:10:37 -0500

    s

--- routes/loadUser.php
+++ routes/loadUser.php
@@ -6,14 +6,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['document'])) {
         $document = $_POST['document'];
         $response = [];
 
-        $sql = "SELECT name, email, phone, id FROM Users WHERE document = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $document);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        if (!preg_match('/^\d{8}$|^d{11}$/', $document)) {
+            throw new InvalidArgumentException('Invalid document format. Only digits are allowed.');
+        }
 
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
+        $sql = "SELECT name, email, phone, id FROM Users WHERE document = :document";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':document', $document, PDO::PARAM_STR);
+        $stmt->execute()
+
+        if ($stmt->rowCount() === 1) {
+            $row = $stmt->fetch(PDO::ASSOC);
             $response['success'] = [
                 'name' => $row['name'], 
                 'email' => $row['email'], 
@@ -21,17 +24,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['document'])) {
                 'id' => $row['id']
             ];
         } else {
-            $response['success'] = false;
+            $response['error'] = 'Document not found';
         }
-        echo json_encode($response);
+        echo json_encode($response, JSON_THROW_ON_ERROR);
     } catch (Exception $e) {
         $response['error'] = $e->getMessage();
-        echo json_encode($response);
+        echo json_encode($response, JSON_THROW_ON_ERROR);
     } finally {
         $stmt->close();
         $conn = null;
     }  
 } else {
     $response['error'] = 'Error Interno';
-    echo json_encode($response);
+    echo json_encode($response, JSON_THROW_ON_ERROR);
 }
