7643ef415cc0ab2159409981b3471319d04df04c
Kenny Muñoz <kenny@munoz.pe>
2024-03-05 17:50:30 -0500

    s

--- includes/bar/topBar_admin.php
+++ includes/bar/topBar_admin.php
@@ -2,13 +2,15 @@
     <div class="wrapper"> 
         <a href="/grid" class="home"><img src="assets/img/home.svg" width="16" height="16" alt="ico">Krear 3D</a>
         <div id="userMenu">
+            <span>Hola, Kenny Muñoz</span>
             <img class="image" src="assets/img/profile.webp" width="16" height="16">
-            <span>Kenny Muñoz</span>
             <div id="userMenuExtented">
                 <div class="content">
                     <img src="assets/img/profile.webp" width="80" height="80">
-                    <span>Kenny Muñoz</span>
-                    <p>Coordinador de Sistemas</p>
+                    <div>
+                        <span>Kenny Muñoz</span>
+                        <p>Coordinador de Sistemas</p>
+                    </div>
                     <a href="proLogout">Cerrar Sesión</a>
                 </div>
             </div>
