8b13588d804948ef5e4e283947855dc4441b0d70
Kenny Muñoz <kenny@munoz.pe>
2024-03-05 15:09:57 -0500

    s

--- includes/app/globals.php
+++ includes/app/globals.php
@@ -1,4 +1,4 @@
 <?php
-$GLOBALS['ver'] = "0.015";
+$GLOBALS['ver'] = "0.017";
 date_default_timezone_set('America/Bogota');
 setlocale(LC_TIME, 'es_ES');
--- includes/bar/navigationBar_admin.php
+++ includes/bar/navigationBar_admin.php
@@ -2,13 +2,10 @@
     <div class="wrapper">
         <a href="/"><img class="logo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
         <ul>
-            <li><a <?php if($currentPage == 'Citas') echo 'class="active"'; ?> href="/">Consultar</a></li>
-            <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="capacitaciones">Capacitaciones</a></li>
-            <!--
-            <li><a <?php //if($currentPage == 'Conocimiento') echo 'class="active"'; ?> href="#">Conocimiento</a></li>
-            <li><a <?php //if($currentPage == 'Cursos') echo 'class="active"'; ?> href="#">Cursos</a></li>
-            -->
+            <li><a <?php if($currentPage == 'Taller') echo 'class="active"'; ?> href="/">Taller</a></li>
+            <li><a <?php if($currentPage == 'Dashboard') echo 'class="active"'; ?> href="reporte">Dashboard</a></li>
+            <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="training">Capacitaciones</a></li>
         </ul>
-        <a class="link" href="https://tiendakrear3d.com/">Tienda<img class="hdr-wsp" width="16" height="16" src="assets/img/tnd.svg" alt="ico"></a>
+        <a class="link" href="/">Soporte<img class="hdr-wsp" width="16" height="16" src="assets/img/home.svg" alt="ico"></a>
     </div>
 </header>
