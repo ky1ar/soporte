09a5ea25340bfca906d4b675ce755727c25c9984
Kenny Muñoz <kenny@munoz.pe>
2024-03-05 17:47:47 -0500

    s

--- includes/bar/navigationBar_admin.php
+++ includes/bar/navigationBar_admin.php
@@ -5,7 +5,7 @@
             <li><a <?php if($currentPage == 'Taller') echo 'class="active"'; ?> href="/grid">Taller</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="training">Capacitaciones</a></li>
             <?php if( $s_levels == 3 || $s_levels == 4 ):?>
-                <li><a <?php if($currentPage == 'Historial') echo 'class="active"'; ?> href="reporte">Historial</a></li>
+                <li><a <?php if($currentPage == 'Historial') echo 'class="active"'; ?> href="report">Historial</a></li>
                 <li><a <?php if($currentPage == 'Técnicos') echo 'class="active"'; ?> href="#">Técnicos</a></li>
                 <li><a <?php if($currentPage == 'Clientes') echo 'class="active"'; ?> href="#">Clientes</a></li>
             <?php endif;?>
