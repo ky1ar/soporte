bc6528606a2ea63293c3abb6cd1b0f11a93f7297
Kenny Muñoz <kenny@munoz.pe>
2024-03-05 15:16:46 -0500

    s

--- includes/app/globals.php
+++ includes/app/globals.php
@@ -1,4 +1,4 @@
 <?php
-$GLOBALS['ver'] = "0.017";
+$GLOBALS['ver'] = "0.018";
 date_default_timezone_set('America/Bogota');
 setlocale(LC_TIME, 'es_ES');
--- includes/bar/navigationBar_admin.php
+++ includes/bar/navigationBar_admin.php
@@ -2,10 +2,12 @@
     <div class="wrapper">
         <a href="/"><img class="logo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
         <ul>
-            <li><a <?php if($currentPage == 'Taller') echo 'class="active"'; ?> href="/">Taller</a></li>
-            <li><a <?php if($currentPage == 'Dashboard') echo 'class="active"'; ?> href="reporte">Dashboard</a></li>
+            <li><a <?php if($currentPage == 'Taller') echo 'class="active"'; ?> href="/grid">Taller</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="training">Capacitaciones</a></li>
+            <li><a <?php if($currentPage == 'Historial') echo 'class="active"'; ?> href="reporte">Historial</a></li>
+            <li><a <?php if($currentPage == 'Técnicos') echo 'class="active"'; ?> href="#">Técnicos</a></li>
+            <li><a <?php if($currentPage == 'Clientes') echo 'class="active"'; ?> href="#">Clientes</a></li>
         </ul>
-        <a class="link" href="/">Soporte<img class="hdr-wsp" width="16" height="16" src="assets/img/home.svg" alt="ico"></a>
+        <a class="link" href="/">Ayuda<img class="hdr-wsp" width="16" height="16" src="assets/img/home.svg" alt="ico"></a>
     </div>
 </header>
