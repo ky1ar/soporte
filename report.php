b4e055fc35079195f39fff1ee55faefe5e70ce83
Kenny Muñoz <kenny@munoz.pe>
2024-03-08 14:21:46 -0500

    s

--- report.php
+++ report.php
@@ -117,7 +117,7 @@ $s_role = $_SESSION['user_role'];
                             <th>Tipo</th>
                             <th>Origen</th>
                             <th>Estado</th>
-                            <th></th>
+                            <th>Ver</th>
                         </tr>
                         <?php 
                         $sql = "SELECT o.id, o.number as orders, o.dates, DATEDIFF(NOW(), o.dates) as pday, c.name, o.comments, c.email, c.document, c.phone, w.id as wid, w.name as wnm, m.model, b.name as bnm, m.slug, t.name as tnm, r.name as onm, o.state FROM Orders o INNER JOIN Machine m ON o.machine = m.id INNER JOIN Users c ON o.client = c.id INNER JOIN Brand b ON m.brand = b.id INNER JOIN Users w ON o.worker = w.id INNER JOIN Type t ON o.type = t.id INNER JOIN Origin r ON o.origin = r.id ORDER BY pday ASC;";
@@ -140,14 +140,169 @@ $s_role = $_SESSION['user_role'];
                                 <td class="row-spn"><span><?php echo $row['onm'] ?></span></td>
                                 <td class="row-stt"><?php echo $row['state'] == 9 ? '<span class="stt-fns">Finalizado</span>':'<span>Activo</span>' ?></td>
                                 <td class="row-act"><img class="tbl-tec" src="assets/img/dot.svg" alt=""></td>
-
                             </tr>
                             <?php $n++;
                             endwhile;
                         endif; ?>
                     </table>
 
+                    <div id="itm-tml">
+
+                <ul class="itm-ul" >
+                    <?php $sql = "SELECT o.id, o.number as orders, o.dates, o.paid, o.dates as pday, c.name, o.comments, c.email, c.document, c.phone, w.id as wid, w.name as wnm, m.model, b.name as bnm, m.slug, t.id as tid, r.id as rid, t.name as tnm, r.name as onm, s.name as snm FROM Orders o INNER JOIN Machine m ON o.machine = m.id INNER JOIN Users c ON o.client = c.id INNER JOIN Brand b ON m.brand = b.id INNER JOIN Users w ON o.worker = w.id INNER JOIN Type t ON o.type = t.id INNER JOIN Status s ON o.state = s.id INNER JOIN Origin r ON o.origin = r.id ORDER BY pday ASC;";
+                    $result = $conn->query($sql);
                     
+                    if ($result->num_rows > 0):
+                        while ($row = $result->fetch_assoc()): 
+                            $pday = getPday($row['pday'], $tday, $fday);
+                            ?>
+                            <li class="itm-li" data-id="<?php echo $row['orders'] ?>">
+                                <img class="itm-cls" src="assets/img/x.svg" alt="">
+                                <div class="itm-smy">
+                                    <form class="smy-lft" method="post">
+                                        <div class="itm-hdr">
+                                            <div class="itm-lft">
+                                                <div class="itm-wfl">
+                                                    <h2>Orden <b>00</b><?php echo $row['orders'] ?></h2>
+                                                    <div class="itm-wrk">
+                                                        <img src="assets/img/tec.svg" alt="">
+                                                        <?php if( $s_levels == 3 || $s_levels == 4 ):?>
+                                                        <select name="worker" class="ky1-wrk">
+                                                            <?php
+                                                            $sqlU = "SELECT w.id, w.name FROM Users w WHERE w.levels = 2 OR w.levels = 3 ORDER BY w.name ";
+                                                            $resultU = $conn->query($sqlU);
+                                                            $p = 1;
+                                                            while ($rowU = $resultU->fetch_assoc()): ?>
+                                                                <option <?php echo $row['wid'] == $rowU['id'] ? 'selected':'' ?> value="<?php echo $rowU['id'] ?>"><?php echo $rowU['name'] ?></option>
+                                                            <?php $p++;
+                                                            endwhile;?>
+                                                        </select> 
+                                                        <?php else: ?>
+                                                            <h3><?php echo $row['wnm'] ?></h3>
+                                                        <?php endif; ?>
+                                                    </div>
+                                                </div>
+                                                <?php if( $s_levels == 3 || $s_levels == 4 ): ?>
+                                                    <div class="itm-box">
+                                                        <input type="hidden" class="ky1-oid" name="orders" value="<?php echo $row['id'] ?>">
+                                                        <button type="submit" class="edt-yes" name="submit"><img src="assets/img/sav.svg" alt=""></button>
+                                                        <button class="itm-btn"><img src="assets/img/edt.svg" alt=""></button>
+                                                        <button class="itm-btn"><img src="assets/img/edt.svg" alt=""></button>
+                                                    </div>
+                                                <?php endif; ?>
+                                            </div>
+                                            <span><?php echo $pday ?> días</span>
+                                        </div>
+                                        <div class="itm-crd">
+                                            <div class="imt-dat">
+                                                <h3><?php echo $row['bnm'] . ' ' . $row['model']?></h3>
+                                                <h2><?php echo $row['name'] ?></h2>
+                                                <h3><?php echo $row['document'] ?></h3>
+                                                <div class="itm-lnk">
+                                                    <p><?php echo $row['email'] ?></p>
+                                                    <a class="itm-wsp" href="https://api.whatsapp.com/send?phone=51<?php echo $row['phone'] ?>" target="_blank" rel="nofollow"><?php echo implode(' ', str_split($row['phone'], 3)) ?><img src="assets/img/wsp2.svg" alt=""></a>
+                                                    <p><?php echo $row['comments'] ?></p>
+                                                </div>
+                                                <div class="itm-otr">
+                                                <?php if( $s_levels == 3 || $s_levels == 4 ):?>
+                                                    <select name="type" class="ky1-typ">
+                                                        <?php
+                                                        $sqlU = "SELECT t.id, t.name FROM Type t ORDER BY t.name ";
+                                                        $resultU = $conn->query($sqlU);
+                                                        $p = 1;
+                                                        while ($rowU = $resultU->fetch_assoc()): ?>
+                                                            <option <?php echo $row['tid'] == $rowU['id'] ? 'selected':'' ?> value="<?php echo $rowU['id'] ?>"><?php echo $rowU['name'] ?></option>
+                                                        <?php $p++;
+                                                        endwhile;?>
+                                                    </select> 
+                                                    <select name="origin" class="ky1-ori">
+                                                        <?php
+                                                        $sqlW = "SELECT o.id, o.name FROM Origin o ORDER BY o.name ";
+                                                        $resultW = $conn->query($sqlW);
+                                                        $p = 1;
+                                                        while ($rowU = $resultW->fetch_assoc()): ?>
+                                                            <option <?php echo $row['rid'] == $rowU['id'] ? 'selected':'' ?> value="<?php echo $rowU['id'] ?>"><?php echo $rowU['name'] ?></option>
+                                                        <?php $p++;
+                                                        endwhile;?>
+                                                    </select> 
+                                                    <?php else: ?>
+                                                        <h5><?php echo $row['tnm'] ?></h5>
+                                                        <h5><?php echo $row['onm'] ?></h5>
+                                                    <?php endif; ?>
+                                                    
+                                                    <select name="type" class="ky1-pid">
+                                                        <option <?php echo $row['paid'] == 0 ? 'selected':'' ?> value="0">No pagado</option>
+                                                        <option <?php echo $row['paid'] == 1 ? 'selected':'' ?> value="1">Pagado</option>
+                                                    </select> 
+                                                </div>
+                                            </div>
+                                            <img class="itm-img" src="assets/mac/<?php echo $row['slug'] ?>.webp" alt="">
+                                        </div>
+                                        <span class="smy-stt">Estado actual de la orden:<b>En <?php echo $row['snm'] ?></b></span>
+                                        <div class="smy-tme">
+                                            <h2>Total de días desde el ingreso</h2>
+                                            <div class="tme-cnt">
+                                                <div id="tme-pdy" class="tme-top" data-stt="<?php echo $pday ?>"><b><?php echo $pday ?> días</b></div>
+                                                <div class="tme-bot"></div>
+                                                <b>0</b>
+                                                <b>20</b>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <div class="smy-rgt">
+                                        <ul>
+                                            <?php 
+                                            $sql2 = "SELECT s.name as snm, os.notes, c.name as cnm, os.dates FROM Orders_Status os INNER JOIN Status s ON os.stat = s.id INNER JOIN Users c ON os.changer = c.id  WHERE os.orders = $row[id];";
+                                            $result2 = $conn->query($sql2);
+                                            $n = 1;
+                                            $t = $result2->num_rows;
+                                            while ($row2 = $result2->fetch_assoc()):
+                                                setlocale(LC_TIME, 'es_ES');
+                                                $date = strtotime($row2['dates']);
+                                                $date = strftime("%e de %B de %Y", $date);
+                                                ?>
+                                                <li class="<?php echo $n == $t ? 'smy-crr':'smy-act' ?>">
+                                                    <i><?php echo $n ?></i>
+                                                    <div class="hst-cnt">
+                                                        <img src="assets/img/<?php echo $stt_img[$n-1] ?>.svg" alt="">
+                                                        <div class="hst-ttl">
+                                                            <h3><?php echo $row2['snm'] ?><em>- <?php echo $row2['cnm'] ?></em></h3>
+                                                            <p><?php echo $row2['notes'] ? ($row2['notes']):'' ?></p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="hst-dte"><?php echo $date ?></div>
+                                                </li>
+                                            <?php $n++;
+                                            endwhile; 
+
+                                            $sql3 = "SELECT * FROM Status WHERE id > $t";
+                                            $result3 = $conn->query($sql3);
+                                            while ($row3 = $result3->fetch_assoc()):?>
+                                                <li>
+                                                    <i><?php echo $n ?></i>
+                                                
+                                                    <div class="hst-cnt">
+                                                    <img src="assets/img/<?php echo $stt_img[$n-1] ?>.svg" alt="">
+                                                        <div class="hst-ttl">
+                                                            <h3><?php echo $row3['name'] ?></h3>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            <?php $n++;
+                                            endwhile;
+                                            ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php 
+                        endwhile;
+                    endif;
+                    ?>
+                </ul>
+            </div>
+
                 </div>
             </div>
         </div>
