a261e88c269a06ce5b3c2856a8f2bc24b8f20be7
Kenny Muñoz <kenny@munoz.pe>
2024-03-11 12:06:10 -0500

    s

--- training.php
+++ training.php
@@ -19,45 +19,10 @@ $stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
 ?>
 </head>
 <body class="ky1-adm">
-    
-    <div id="ky1-bar">
-        <div class="ky1-wrp bar-enl">	 
-            <a href="https://api.whatsapp.com/send?phone=51934760404" target="_blank" rel="nofollow"><img src="https://ky1arsupport.tiendakrear3d.com/wp-content/uploads/2023/08/wsp.svg" width="16" height="16" alt="ico">934 760 404</a>
-            <a href="https://api.whatsapp.com/send?phone=51982001288" target="_blank" rel="nofollow"><img src="https://ky1arsupport.tiendakrear3d.com/wp-content/uploads/2023/08/wsp.svg" width="16" height="16" alt="ico">982 001 288</a>
-            <a href="mailto:ventas2@krear3d.com" target="_blank" rel="nofollow"><img src="https://ky1arsupport.tiendakrear3d.com/wp-content/uploads/2023/08/eml.svg" width="16" height="16" alt="ico">ventas2@krear3d.com</a>
-            <a href="https://bit.ly/2ZzWUeK" target="_blank" rel="nofollow"><img src="https://ky1arsupport.tiendakrear3d.com/wp-content/uploads/2023/08/map.svg" width="16" height="16" alt="ico">Calle Javier Fernández 262 Miraflores - Lima</a>
-        </div>
-    </div>
-    <header id="ky1-hdr-adm" class="ky1-blr">
-        <div class="ky1-wrp">
-            <div class="ky1-lft">
-                <div id="hdr-usr">
-                    <img class="hdr-prf" src="assets/img/profile.webp" width="40" height="40" alt="Perfil">
-                    <div class="ky1-txt">
-                        <span><?php echo $s_name ?></span>
-                        <p><?php echo $s_role ?></p>
-                    </div>
-                    <div id="hdr-ext">
-                        <div class="ext-cnt">
-                            <img class="hdr-prf" src="assets/img/profile.webp" width="80" height="80" alt="Perfil">
-                            <span><?php echo $s_name ?></span>
-                            <a href="proLogout">Cerrar Sesión</a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="flt-usr flt-act" data-usrf="1"><img src="assets/img/edt.svg" alt="">Dashboard</div>
-                <div class="flt-usr flt-act" data-usrf="2"><img src="assets/img/usr.svg" alt="">Técnicos</span></div>
-            </div>
-            <div class="ky1-rgt">
-                <a href="https://api.whatsapp.com/send?phone=51946887982" target="_blank" rel="nofollow"><img src="assets/img/cnt.svg" alt=""></a>
-                <?php if( $s_levels == 3 || $s_levels == 4 ):?>
-                    <a href="grid"><img src="assets/img/adm.svg" alt=""></a>
-                <?php endif;?>
-            </div>
-        </div>
-    </header>
-
+    <?php 
+    require_once 'includes/bar/topBar_admin.php';
+    require_once 'includes/bar/navigationBar_admin.php'; 
+    ?>
     <section id="adminSection">
         <div class="ky1-wrp">
         <?php 
