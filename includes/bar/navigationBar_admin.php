5ac120b825635060946bd3a3f35ab17564d61db9
Kenny Muñoz <kenny@munoz.pe>
2024-03-11 11:43:35 -0500

    s

--- includes/bar/navigationBar_admin.php
+++ includes/bar/navigationBar_admin.php
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <a href="/"><img class="logo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
         <ul>
-            <li><a <?php if($currentPage == 'Taller') echo 'class="active"'; ?> href="/grid">Taller</a></li>
+            <li><a <?php if($currentPage == 'Soporte Técnico') echo 'class="active"'; ?> href="/grid">Soporte Técnico</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="training">Capacitaciones</a></li>
             <?php if( $s_levels == 3 || $s_levels == 4 ):?>
                 <li><a <?php if($currentPage == 'Historial') echo 'class="active"'; ?> href="report">Historial</a></li>
