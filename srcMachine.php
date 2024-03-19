91e142c48db4d66a857f44d87479caad7dbc8884
Kenny Muñoz <kenny@munoz.pe>
2024-03-19 00:17:15 -0500

    s

--- srcMachine.php
+++ srcMachine.php
@@ -5,13 +5,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['machine'])) {
     $searchTerm = $_POST['machine'];
 
     // Consulta SQL actualizada para buscar por modelo, marca o slug
-    $sql = "SELECT m.id, m.slug, CONCAT(b.name, ' ', m.model) AS fullName FROM Machine m JOIN Brand b ON m.brand = b.id WHERE CONCAT(b.name, ' ', m.model) LIKE '%$searchTerm%'";
-    $result = $conn->query($sql);
+    $sql = "SELECT m.id, m.slug, CONCAT(b.name, ' ', m.model) AS fullName FROM Machine m JOIN Brand b ON m.brand = b.id WHERE CONCAT(b.name, ' ', m.model) LIKE '%$machineVal%'";
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             // Mostrar el resultado con la nueva columna
-            echo "<p class='frm-sgs' data-id='" . $row['machine_id'] . "' data-slug='" . $row['slug'] . "'>" . $row['marca_modelo'] . "</p>";
+            echo "<p class='frm-sgs' data-id='" . $row['id'] . "' data-slug='" . $row['slug'] . "'>" . $row['fullName'] . "</p>";
         }
     } else {
         echo "<p class='frm-sgs'>No se encontraron resultados</p>";
