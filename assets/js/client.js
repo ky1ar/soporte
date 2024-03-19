ccf688c836298aca50f8bf6d2047de9f848956ed
Kenny Muñoz <kenny@munoz.pe>
2024-03-19 02:37:07 -0500

    s

--- assets/js/client.js
+++ assets/js/client.js
@@ -314,7 +314,7 @@ $(document).ready(function () {
     return true;
   }
   function validatePhone(phone) {
-    var telefonoRegex = /^\d+$/;
+    var telefonoRegex = /^(\+\d+)?\d+$/;
     if (!telefonoRegex.test(phone)) {
       message(scheduleFormMessage, "Ingrese un número de teléfono válido");
       return false;
--- includes/app/globals.php
+++ includes/app/globals.php
@@ -1,4 +1,4 @@
 <?php
-$GLOBALS['ver'] = "0.129";
+$GLOBALS['ver'] = "0.130";
 date_default_timezone_set('America/Bogota');
 setlocale(LC_TIME, 'es_ES');
