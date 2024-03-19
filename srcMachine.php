ae05646b1fd6d4220ed324e41216b2252c0cd842
Kenny Muñoz <kenny@munoz.pe>
2024-03-19 00:18:06 -0500

    s

--- srcMachine.php
+++ srcMachine.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['machine'])) {
 
     // Consulta SQL actualizada para buscar por modelo, marca o slug
     $sql = "SELECT m.id, m.slug, CONCAT(b.name, ' ', m.model) AS fullName FROM Machine m JOIN Brand b ON m.brand = b.id WHERE CONCAT(b.name, ' ', m.model) LIKE '%$searchTerm%'";
-
+    $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             // Mostrar el resultado con la nueva columna
