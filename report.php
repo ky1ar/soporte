7d9c8e2385d64b8b385ba79843c450ed075c5e48
Kenny Muñoz <kenny@munoz.pe>
2024-04-01 12:00:28 -0500

    s

--- report.php
+++ report.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: krear3dperu");
@@ -13,63 +13,25 @@ $s_role = $_SESSION['user_role'];
 
 $currentPage = "H. de Equipos"; 
 require_once 'includes/app/db.php';
-require_once 'includes/app/globals.php'; 
+require_once 'includes/app/globals.php';
 require_once 'includes/common/header_admin.php';
-$stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin']; 
+$stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
 ?>
 </head>
 <body class="ky1-adm">
-    <?php 
+    <?php
     require_once 'includes/bar/topBar_admin.php';
-    require_once 'includes/bar/navigationBar_admin.php'; 
+    require_once 'includes/bar/navigationBar_admin.php';
     ?>
     <section id="rpt-dat">
         <div class="wrapper">
-            <div class="dat-pad">
-                <ul class="dat-one">
-                    <li class="lft-a">
-                        <div class="cnt-lft">
-                            <?php $sql = "SELECT id FROM Orders WHERE state != 9";
-                            $result = $conn->query($sql);
-                            $row = $result->fetch_assoc();
-                            $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>En Actividad</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/wip.svg" alt="">
-                        
-                    </li>
-                    <li class=" lft-b">
-                        <div class="cnt-lft">
-                            <?php $sql = "SELECT id FROM Orders WHERE MONTH(dates) = MONTH(CURDATE()) AND YEAR(dates) = YEAR(CURDATE())";
-                            $result = $conn->query($sql);
-                            $row = $result->fetch_assoc();
-                            $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>En este Mes</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/cal2.svg" alt="">
-                    </li>
-                    <li class="lft-c">
-                        <div class="cnt-lft">
-                        <?php $sql = "SELECT id FROM Orders WHERE state = 9";
-                        $result = $conn->query($sql);
-                        $row = $result->fetch_assoc();
-                        $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>Completados</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/fns.svg" alt="">
-                    </li>
-                </ul>
-            </div>
             <div class="dat-pad">
                 <div class="dat-two">
                     <div class="two-hdr">
                         <h3>Historial de Ordenes<p>Listado de equipos ingresados al área de Soporte Técnico</p></h3>
                         <a href="#"><img src="assets/img/pls.svg" alt="" style="width: 1rem;">Añadir Orden</a>
                     </div>
-                    <table class="rpt-tbl" border="0" cellspacing="0" cellpadding="0">
+                    <table aria-describedby="Training Report" class="rpt-tbl" border="0">
                         <tr class="row-hdr">
                             <th>No.</th>
                             <th>Orden</th>
@@ -80,8 +42,32 @@ $stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
                             <th>Estado</th>
                             <th></th>
                         </tr>
-                        <?php 
-                        $sql = "SELECT o.id, o.number as orders, o.dates, DATEDIFF(NOW(), o.dates) as pday, c.name, o.comments, c.email, c.document, c.phone, w.id as wid, w.name as wnm, m.model, b.name as bnm, m.slug, t.name as tnm, r.name as onm, o.state FROM Orders o INNER JOIN Machine m ON o.machine = m.id INNER JOIN Users c ON o.client = c.id INNER JOIN Brand b ON m.brand = b.id INNER JOIN Users w ON o.worker = w.id INNER JOIN Type t ON o.type = t.id INNER JOIN Origin r ON o.origin = r.id ORDER BY pday ASC;";
+                        <?php
+                        $sql = "SELECT o.id,
+                        o.number as orders,
+                        o.dates,
+                        DATEDIFF(NOW(), o.dates) as pday,
+                        c.name,
+                        o.comments,
+                        c.email,
+                        c.document,
+                        c.phone,
+                        w.id as wid,
+                        w.name as wnm,
+                        m.model,
+                        b.name as bnm,
+                        m.slug,
+                        t.name as tnm,
+                        r.name as onm,
+                        o.state
+                        FROM Orders o
+                        INNER JOIN Machine m ON o.machine = m.id
+                        INNER JOIN Users c ON o.client = c.id
+                        INNER JOIN Brand b ON m.brand = b.id
+                        INNER JOIN Users w ON o.worker = w.id
+                        INNER JOIN Type t ON o.type = t.id
+                        INNER JOIN Origin r ON o.origin = r.id
+                        ORDER BY pday ASC;";
                         $result = $conn->query($sql);
                         $n = 1;
                         if ($result->num_rows > 0):
@@ -93,13 +79,27 @@ $stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
                             <tr>
                                 <td><?php echo $n ?></td>
                                 <td class="row-odr">
-                                    <img width="48" class="tbl-img" src="assets/mac/<?php echo $row['slug'] ?>.webp" alt="">
-                                    <div class="tbl-odr"><?php echo $row['orders'] ?><span><?php echo $row['model']?></span></div></td>
+                                    <img
+                                    width="48"
+                                    class="tbl-img"
+                                    src="assets/mac/<?php echo $row['slug'] ?>.webp" alt="">
+                                    <div class="tbl-odr">
+                                        <?php echo $row['orders'] ?><span><?php echo $row['model']?></span>
+                                    </div>
+                                </td>
                                 <td><?php echo $row['wnm'] ?></td>
                                 <td><?php echo $date ?></td>
                                 <td class="row-spn"><span><?php echo $row['tnm'] ?></span></td>
                                 <td class="row-spn"><span><?php echo $row['onm'] ?></span></td>
-                                <td class="row-stt"><?php echo $row['state'] == 9 ? '<span class="stt-fns">Finalizado</span>':'<span>Activo</span>' ?></td>
+                                <td class="row-stt">
+                                    <?php
+                                    if ($row['state'] == 9) {
+                                        echo '<span class="stt-fns">Finalizado</span>';
+                                    } else {
+                                        echo '<span>Activo</span>';
+                                    }
+                                    ?>
+                                </td>
                                 <td class="row-act"><img class="tbl-tec" src="assets/img/dot.svg" alt=""></td>
 
                             </tr>
@@ -107,13 +107,9 @@ $stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
                             endwhile;
                         endif; ?>
                     </table>
-
-                    
                 </div>
             </div>
         </div>
     </section>
-
-   
 </body>
 </html>
--- training_report.php
+++ training_report.php
@@ -11,109 +11,86 @@ $s_name = $_SESSION['user_name'];
 $s_nick = $_SESSION['user_nick'];
 $s_role = $_SESSION['user_role'];
 
-$currentPage = "H. de Capacitaciones"; 
+$currentPage = "H. de Capacitaciones";
 require_once 'includes/app/db.php';
-require_once 'includes/app/globals.php'; 
+require_once 'includes/app/globals.php';
 require_once 'includes/common/header_admin.php';
-$stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin']; 
+$stt_img = ['one', 'two', 'thr', 'for', 'fiv', 'six', 'sev', 'eig', 'nin'];
 ?>
 </head>
 <body class="ky1-adm">
-    <?php 
+    <?php
     require_once 'includes/bar/topBar_admin.php';
-    require_once 'includes/bar/navigationBar_admin.php'; 
+    require_once 'includes/bar/navigationBar_admin.php';
     ?>
     <section id="rpt-dat">
         <div class="wrapper">
             <div class="dat-pad">
-                <ul class="dat-one">
-                    <li class="lft-a">
-                        <div class="cnt-lft">
-                            <?php $sql = "SELECT id FROM Orders WHERE state != 9";
-                            $result = $conn->query($sql);
-                            $row = $result->fetch_assoc();
-                            $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>En Actividad</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/wip.svg" alt="">
-                        
-                    </li>
-                    <li class=" lft-b">
-                        <div class="cnt-lft">
-                            <?php $sql = "SELECT id FROM Orders WHERE MONTH(dates) = MONTH(CURDATE()) AND YEAR(dates) = YEAR(CURDATE())";
-                            $result = $conn->query($sql);
-                            $row = $result->fetch_assoc();
-                            $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>En este Mes</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/cal2.svg" alt="">
-                    </li>
-                    <li class="lft-c">
-                        <div class="cnt-lft">
-                        <?php $sql = "SELECT id FROM Orders WHERE state = 9";
-                        $result = $conn->query($sql);
-                        $row = $result->fetch_assoc();
-                        $c = $result->num_rows;?>
-                            <h2><?php echo $c ?><span>Servicios</span></h2>
-                            <p>Completados</p>
-                        </div>
-                        <img class="tml-img" src="assets/img/fns.svg" alt="">
-                    </li>
-                </ul>
-            </div>
-            <div class="dat-pad">
                 <div class="dat-two">
                     <div class="two-hdr">
-                        <h3>Historial de Ordenes<p>Listado de equipos ingresados al área de Soporte Técnico</p></h3>
+                        <h3>Historial de Capacitaciones
+                            <p>Listado de usuarios y solicitudes de capacitación</p>
+                        </h3>
                         <a href="#"><img src="assets/img/pls.svg" alt="" style="width: 1rem;">Añadir Orden</a>
                     </div>
-                    <table class="rpt-tbl" border="0" cellspacing="0" cellpadding="0">
+                    <table aria-describedby="Training Report" class="rpt-tbl" border="0">
                         <tr class="row-hdr">
-                            <th>No.</th>
-                            <th>Orden</th>
+                            <th>Solicitud</th>
+                            <th>Equipo</th>
                             <th>Técnico</th>
-                            <th>Ingreso</th>
-                            <th>Tipo</th>
-                            <th>Origen</th>
+                            <th>Cliente</th>
+                            <th>Fecha</th>
+                            <th>Horario</th>
                             <th>Estado</th>
                             <th></th>
                         </tr>
-                        <?php 
-                        $sql = "SELECT o.id, o.number as orders, o.dates, DATEDIFF(NOW(), o.dates) as pday, c.name, o.comments, c.email, c.document, c.phone, w.id as wid, w.name as wnm, m.model, b.name as bnm, m.slug, t.name as tnm, r.name as onm, o.state FROM Orders o INNER JOIN Machine m ON o.machine = m.id INNER JOIN Users c ON o.client = c.id INNER JOIN Brand b ON m.brand = b.id INNER JOIN Users w ON o.worker = w.id INNER JOIN Type t ON o.type = t.id INNER JOIN Origin r ON o.origin = r.id ORDER BY pday ASC;";
+                        <?php
+                        $sql = "SELECT
+                        t.id as training_id,
+                        m.slug as machine_slug,
+                        m.model as machine_model,
+                        w.name as worker_name,
+                        t.name as client_name,
+                        training_date,
+                        training_start,
+                        training_state
+                        FROM Training t
+                        INNER JOIN Machine m ON t.machine = m.id
+                        INNER JOIN Users w ON t.worker = w.id
+                        ORDER BY t.id DESC";
+
                         $result = $conn->query($sql);
-                        $n = 1;
                         if ($result->num_rows > 0):
                             while ($row = $result->fetch_assoc()):
                                 setlocale(LC_TIME, 'es_ES');
-                                $date = strtotime($row['dates']);
+                                $date = strtotime($row['training_date']);
                                 $date = strftime("%e de %B de %Y", $date);
                                 ?>
                             <tr>
-                                <td><?php echo $n ?></td>
+                                <td><?php echo $row['training_id'] ?></td>
                                 <td class="row-odr">
-                                    <img width="48" class="tbl-img" src="assets/mac/<?php echo $row['slug'] ?>.webp" alt="">
-                                    <div class="tbl-odr"><?php echo $row['orders'] ?><span><?php echo $row['model']?></span></div></td>
-                                <td><?php echo $row['wnm'] ?></td>
+                                    <img
+                                    width="48"
+                                    class="tbl-img"
+                                    src="assets/mac/<?php echo $row['machine_slug'] ?>.webp" alt="">
+                                    <div class="tbl-odr">
+                                        <span><?php echo $row['machine_model']?></span>
+                                    </div>
+                                </td>
+                                <td><?php echo $row['worker_name'] ?></td>
+                                <td><?php echo $row['client_name'] ?></td>
                                 <td><?php echo $date ?></td>
-                                <td class="row-spn"><span><?php echo $row['tnm'] ?></span></td>
-                                <td class="row-spn"><span><?php echo $row['onm'] ?></span></td>
-                                <td class="row-stt"><?php echo $row['state'] == 9 ? '<span class="stt-fns">Finalizado</span>':'<span>Activo</span>' ?></td>
+                                <td><?php echo $row['training_start'] ?></td>
+                                <td><?php echo $row['training_state'] ?></td>
                                 <td class="row-act"><img class="tbl-tec" src="assets/img/dot.svg" alt=""></td>
-
                             </tr>
-                            <?php $n++;
+                            <?php
                             endwhile;
                         endif; ?>
                     </table>
-
-                    
                 </div>
             </div>
         </div>
     </section>
-
-   
 </body>
 </html>
