4a24aab75053c7e0d47f2c679fe168142f4c0bf0
Kenny Muñoz <kenny@munoz.pe>
2024-04-01 11:19:31 -0500

    s

--- includes/bar/navigationBar_admin.php
+++ includes/bar/navigationBar_admin.php
@@ -2,10 +2,11 @@
     <div class="wrapper">
         <a href="/"><img class="logo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
         <ul>
-            <li><a <?php if($currentPage == 'Soporte Técnico') echo 'class="active"'; ?> href="/grid">Soporte Técnico</a></li>
+            <li><a <?php if($currentPage == 'Equipos') echo 'class="active"'; ?> href="/grid">Equipos</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="training">Capacitaciones</a></li>
             <?php if( $s_levels == 3 || $s_levels == 4 ):?>
-                <li><a <?php if($currentPage == 'Historial') echo 'class="active"'; ?> href="report">Historial</a></li>
+                <li><a <?php if($currentPage == 'H. de Equipos') echo 'class="active"'; ?> href="report">H. de Equipos</a></li>
+                <li><a <?php if($currentPage == 'H. de Capacitaciones') echo 'class="active"'; ?> href="report">H. de Capacitaciones</a></li>
                 <li><a <?php if($currentPage == 'Técnicos') echo 'class="active"'; ?> href="#">Técnicos</a></li>
                 <li><a <?php if($currentPage == 'Clientes') echo 'class="active"'; ?> href="#">Clientes</a></li>
             <?php endif;?>
