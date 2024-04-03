a488823e4b6b20ae6a7d356f1d7c1b81dbc4377a
alex <alex@gmail.com>
2024-04-03 18:05:53 -0500

    edit

---
+++ home.php
@@ -0,0 +1,44 @@
+<?php
+$currentPage = "Test";
+require_once 'includes/app/globals.php';
+require_once 'includes/common/header.php';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+<body>
+    <?php
+    require_once 'includes/bar/topBar.php';
+    require_once 'includes/bar/navigationBar.php';
+    ?>
+    <section id="">
+        <div class="">
+            <img style="width: 100%;" height="630" src="assets/slider/banner-home.png">
+        </div>
+    </section>
+
+    <section class="s1">
+        <div class="section-item">
+            <h1>GARANTIA EXTENDIDA</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos modi, similique dolorem esse tenetur numquam vitae repellendus sapiente eveniet quidem expedita libero laboriosam. Est placeat quibusdam soluta quis maiores.</p>
+            <button>COTIZAR</button>
+        </div>
+        <div class="section-item">
+            <h1>ENTRENAMIENTO CERTIFICADO</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum sequi sed iure recusandae sunt voluptatibus sit officia possimus veniam molestias impedit numquam doloribus ratione qui fugit, magni aperiam! Unde, laudantium!</p>
+            <button>COTIZAR</button>
+        </div>
+    </section>
+
+    <section id="">
+        <div class="">
+            <img style="width: 100%;" height="630" src="assets/slider/banner-home.png">
+        </div>
+    </section>
+</body>
+
+</html>
