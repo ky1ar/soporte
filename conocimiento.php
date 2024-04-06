aa53862a65ad22a93e795c4b9cc65f729c234a1d
alex <alex@gmail.com>
2024-04-06 13:05:34 -0500

    edit

---
+++ conocimiento.php
@@ -0,0 +1,36 @@
+<?php
+$currentPage = "Test";
+require_once 'includes/app/globals.php';
+require_once 'includes/common/header.php';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/conocimiento.css">
+
+</head>
+
+<body>
+    <?php
+    require_once 'includes/bar/topBar.php';
+    require_once 'includes/bar/navigationBar.php';
+    ?>
+    <section id="">
+        <div class="">
+            <img style="width: 100%;" height="630" src="assets/slider/banner_basec.webp">
+        </div>
+    </section>
+    <section style="height: 600px;">
+        <div class="et">
+            <p>TECNOLOGÍA FDM</p>
+        </div>
+    </section>
+
+    <?php require_once 'includes/common/footer.php'; ?>
+
+    <script src="./assets/js/efects.js"></script>
+</body>
+
+</html>
