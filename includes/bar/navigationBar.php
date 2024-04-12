0cad4c7748aa84a8c11bc86c14ad038ac156cfd6
Kenny Muñoz <kenny@munoz.pe>
2024-04-12 10:19:56 -0500

    s

--- includes/bar/navigationBar.php
+++ includes/bar/navigationBar.php
@@ -1,6 +1,6 @@
 <header id="navigationBar">
     <div class="wrapper">
-        <a href="/"><img class="logo" width="150" height="42" src="assets/img/logod.webp" alt="Logo Krear 3D"></a>
+        <a href="/"><img class="logo" width="150" height="42" src="assets/img/logo.svg" alt="Logo Krear 3D"></a>
         <ul>
             <li><a <?php if($currentPage == 'Citas') echo 'class="active"'; ?> href="/">Consultar</a></li>
             <li><a <?php if($currentPage == 'Capacitaciones') echo 'class="active"'; ?> href="capacitaciones">Capacitaciones</a></li>
