0c6e8af3da7970fffcc67430e003167670cd7b86
Kenny Muñoz <kenny@munoz.pe>
2024-04-17 14:47:34 -0500

    s

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -685,7 +685,7 @@ $(document).ready(function () {
           $name.text(data.name);
           console.log(data.invoice);
 
-          $invoice.html('<img width="12" height="12" src="assets/img/invoice.svg" alt="">Comprobante').attr("data-src", data.invoice);
+          viewInvoice.attr("data-src", data.invoice);
           $document.text(data.document);
           $email.text(data.email);
           $phone.text("+" + data.phone).attr("href", "https://api.whatsapp.com/send?phone="+data.phone);
