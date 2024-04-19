0e8151c2cb40861316c50ccaf5007e339ed5721e
alex <alex@gmail.com>
2024-04-19 12:48:25 -0500

    conocimiento

--- conocimiento.php
+++ conocimiento.php
@@ -27,71 +27,26 @@ require_once 'includes/common/header.php';
         <div class="et">
             <p>TECNOLOGÍA FDM</p>
         </div>
-        <div class="carousel-container">
-            <div class="carousel">
-                <div class="carousel-item">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="assets/img/test.jpg" alt="Imagen de ejemplo">
-                        </div>
-                        <div class="card-content">
-                            <div class="card-text">
-                                <img class="icon-search" src="assets/img/search_y1.png" alt="">
-                                <p class="small-text">26 mayo 2024</p>
-                            </div>
-                            <div class="card-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, autem nesciunt rerum error dolor, libero earum expedita nihil illo tempo</p>
-                                <p class="truncate">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur dolor natus, alias animi vitae quisquam illum. Suscipit id aliquid at molestiae, ipsa placeat rem maxime, hic quod explicabo deserunt.</p>
-                            </div>
-                            <button class="card-button">Leer más</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="assets/img/test.jpg" alt="Imagen de ejemplo">
-                        </div>
-                        <div class="card-content">
-                            <div class="card-text">
-                                <img class="icon-search" src="assets/img/search_y1.png" alt="">
-                                <p class="small-text">26 mayo 2024</p>
-                            </div>
-                            <div class="card-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, autem nesciunt rerum error dolor, libero earum expedita nihil illo tempo</p>
-                                <p class="truncate">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur dolor natus, alias animi vitae quisquam illum. Suscipit id aliquid at molestiae, ipsa placeat rem maxime, hic quod explicabo deserunt.</p>
-                            </div>
-                            <button class="card-button">Leer más</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="assets/img/test.jpg" alt="Imagen de ejemplo">
-                        </div>
-                        <div class="card-content">
-                            <div class="card-text">
-                                <img class="icon-search" src="assets/img/search_y1.png" alt="">
-                                <p class="small-text">26 mayo 2024</p>
-                            </div>
-                            <div class="card-description">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, autem nesciunt rerum error dolor, libero earum expedita nihil illo tempo</p>
-                                <p class="truncate">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur dolor natus, alias animi vitae quisquam illum. Suscipit id aliquid at molestiae, ipsa placeat rem maxime, hic quod explicabo deserunt.</p>
-                            </div>
-                            <button class="card-button">Leer más</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="prev" onclick="prevSlide()">Anterior</button>
-            <button class="next" onclick="nextSlide()">Siguiente</button>
-        </div>
-    </section>
-    <section>
         <div class="swiffy-slider slider-item-show4 slider-nav-autoplay" data-slider-nav-autoplay-interval="2500">
             <ul class="slider-container">
-                <li><img src="https://source.unsplash.com/49b9l_29ceA/1600x900" style="max-width: 100%;height: auto;"></li>
+                <li>
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/img/test.jpg" alt="Imagen de ejemplo">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-text">
+                                <img class="icon-search" src="assets/img/search_y1.png" alt="">
+                                <p class="small-text">26 mayo 2024</p>
+                            </div>
+                            <div class="card-description">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, autem nesciunt rerum error dolor, libero earum expedita nihil illo tempo</p>
+                                <p class="truncate">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad aspernatur dolor natus, alias animi vitae quisquam illum. Suscipit id aliquid at molestiae, ipsa placeat rem maxime, hic quod explicabo deserunt.</p>
+                            </div>
+                            <button class="card-button">Leer más</button>
+                        </div>
+                    </div>
+                </li>
                 <li><img src="https://source.unsplash.com/nKAglN6HBH8/1600x900" style="max-width: 100%;height: auto;"></li>
                 <li><img src="https://source.unsplash.com/E9ZwWcMGzj8/1600x900" style="max-width: 100%;height: auto;"></li>
             </ul>
