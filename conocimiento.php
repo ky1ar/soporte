9bf1fcf086ce04c44d8311a2ca724f7df832feb3
alex <alex@gmail.com>
2024-04-19 14:13:19 -0500

    conocimiento

--- conocimiento.php
+++ conocimiento.php
@@ -27,7 +27,7 @@ require_once 'includes/common/header.php';
         <div class="et">
             <p>TECNOLOGÍA FDM</p>
         </div>
-        <div class="swiffy-slider slider-item-show4 slider-item-nogap slider-nav-square slider-nav-animation">
+        <div class="swiffy-slider slider-item-show4 slider-item-nogap slider-nav-round slider-nav-animation">
             <ul class="slider-container">
                 <li>
                     <div class="card">
