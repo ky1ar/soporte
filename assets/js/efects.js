494d76538fc2b8e438746b9edf101a6e7d2cc158
alex <alex@gmail.com>
2024-04-22 11:43:29 -0500

    edt

--- assets/js/efects.js
+++ assets/js/efects.js
@@ -31,9 +31,11 @@ function adjustSlider() {
   var screenWidth = window.innerWidth;
   var sliders = document.querySelectorAll('.swiffy-slider');
   sliders.forEach(function (slider) {
-      if (screenWidth < 1660) {
+      if (screenWidth < 1650) {
           slider.classList.replace('slider-item-show4', 'slider-item-show3');
-      } else {
+      }else if (screenWidth < 1350) {
+        slider.classList.replace('slider-item-show3', 'slider-item-show2');
+      }else {
           slider.classList.replace('slider-item-show3', 'slider-item-show4');
       }
   });
