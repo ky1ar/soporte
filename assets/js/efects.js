3f6b5552dbb2552675ff3e084460ba7802abd169
alex <alex@gmail.com>
2024-04-22 14:03:54 -0500

    edt

--- assets/js/efects.js
+++ assets/js/efects.js
@@ -33,8 +33,8 @@ function adjustSlider() {
   sliders.forEach(function (slider) {
       if (screenWidth < 1650) {
           slider.classList.replace('slider-item-show4', 'slider-item-show3');
-      }if (screenWidth < 1350) {
-        slider.classList.replace('slider-item-show3', 'slider-item-show2');
+      // }else if (screenWidth < 1350) {
+      //   slider.classList.replace('slider-item-show3', 'slider-item-show2');
       }else {
           slider.classList.replace('slider-item-show3', 'slider-item-show4');
       }
