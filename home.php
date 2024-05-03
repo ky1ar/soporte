b00071143185d99b79b468d662175ff9887ab8fb
alex <alex@gmail.com>
2024-05-03 16:05:03 -0500

    first

--- home.php
+++ home.php
@@ -22,10 +22,9 @@ require_once 'includes/common/header.php';
             <img style="width: 100%;" height="630" src="assets/slider/banner-home.webp">
         </div>
     </section>
-
     <section class="s1">
         <div class="section-item">
-            <h1>GARANTIA EXTENDIDA</h1>
+            <h1>GARANTIA EXTENDIDAx</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quos modi, similique dolorem esse tenetur numquam vitae repellendus sapiente eveniet quidem expedita libero laboriosam. Est placeat quibusdam soluta quis maiores.</p>
             <button>COTIZAR</button>
         </div>
