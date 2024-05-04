86363fb5ad602104b7f453f6dac5f909c857e7b7
alex <alex@gmail.com>
2024-05-04 13:07:19 -0500

    first

--- home.php
+++ home.php
@@ -6,7 +6,7 @@ require_once 'includes/common/header.php';
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/home.css">
 
