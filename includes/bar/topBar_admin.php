c5cddec2f2d65e3700032796743df32ce67a87bc
alex <alex@gmail.com>
2024-05-06 13:12:26 -0500

    edit

--- includes/bar/topBar_admin.php
+++ includes/bar/topBar_admin.php
@@ -1,19 +1,20 @@
+<div id="userMenuExtented">
+    <div class="content">
+        <img alt="" src="assets/img/profile.webp" width="80" height="80">
+        <div>
+            <span>Kenny Muñoz</span>
+            <p>Coordinador de Sistemas</p>
+        </div>
+        <a href="proLogout">Cerrar Sesión</a>
+    </div>
+</div>
 <div id="topBar">
     <div class="wrapper">
         <a href="/grid" class="home"><img src="assets/img/home.svg" width="16" height="16" alt="ico">Krear 3D</a>
         <div id="userMenu">
             <span>Hola, Kenny Muñoz</span>
             <img alt="" class="image" src="assets/img/profile.webp" width="16" height="16">
-            <div id="userMenuExtented">
-                <div class="content">
-                    <img alt="" src="assets/img/profile.webp" width="80" height="80">
-                    <div>
-                        <span>Kenny Muñoz</span>
-                        <p>Coordinador de Sistemas</p>
-                    </div>
-                    <a href="proLogout">Cerrar Sesión</a>
-                </div>
-            </div>
+
         </div>
     </div>
 </div>
