9a24714cae217d23e14d7bfa7be68da291fdba38
alex <alex@gmail.com>
2024-05-07 11:44:22 -0500

    edit

--- home.php
+++ home.php
@@ -96,20 +96,22 @@ require_once 'includes/common/header.php';
     </section>
     <section class="home-contact">
         <h1>CONTÁCTANOS</h1>
-        <div class="card-info">
-            <h1>Email</h1>
-            <img src="assets/img/home-email.png" alt="">
-            <p>soporte@krear3d.com</p>
-        </div>
-        <div class="card-info">
-            <h1>WhatsApp</h1>
-            <img src="assets/img/whatsapp.png" alt="">
-            <p>+51 970 539 751</p>
-        </div>
-        <div class="card-info">
-            <h1>Encuéntranos</h1>
-            <img src="assets/img/home-ubi.png" alt="">
-            <p>Calle Taturno 116, Surco</p>
+        <div>
+            <div class="card-info">
+                <h1>Email</h1>
+                <img src="assets/img/home-email.png" alt="">
+                <p>soporte@krear3d.com</p>
+            </div>
+            <div class="card-info">
+                <h1>WhatsApp</h1>
+                <img src="assets/img/whatsapp.png" alt="">
+                <p>+51 970 539 751</p>
+            </div>
+            <div class="card-info">
+                <h1>Encuéntranos</h1>
+                <img src="assets/img/home-ubi.png" alt="">
+                <p>Calle Taturno 116, Surco</p>
+            </div>
         </div>
     </section>
     <?php require_once 'includes/common/footer.php'; ?>
