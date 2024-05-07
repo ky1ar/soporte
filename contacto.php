48a596764e64814740fc105fcd86ae171dc4d6bc
alex <alex@gmail.com>
2024-05-07 17:33:03 -0500

    edit

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<?php
+$currentPage = "Test";
+require_once 'includes/app/globals.php';
+require_once 'includes/common/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require_once 'includes/bar/topBar.php';
+    require_once 'includes/bar/navigationBar.php';
+    ?>
+    <section>
+        hola
+    </section>
+     <?php require_once 'includes/common/footer.php'; ?>
+</body>
+
+</html>
