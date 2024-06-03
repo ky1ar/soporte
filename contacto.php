22c788c6951928e61c5d04f5c08dbc2a37665009
alex <alex@gmail.com>
2024-06-03 17:46:44 -0500

    test

--- contacto.php
+++ contacto.php
@@ -14,21 +14,27 @@ require_once 'includes/common/header.php';
         <img src="assets/img/banner-contacto-pc.webp">
     </section>
     <section class="contact-info">
-        <div class="card">
-            <h1>Email</h1>
-            <img src="assets/img/home-email.png" alt="">
-            <p>soporte@krear3d.com</p>
-        </div>
-        <div class="card">
-            <h1>WhatsApp</h1>
-            <img src="assets/img/whatsapp.png" alt="">
-            <p>+51 970 539 751</p>
-        </div>
-        <div class="card">
-            <h1>Encuéntranos</h1>
-            <img src="assets/img/home-ubi.png" alt="">
-            <p>Calle Tutumo 116, Surco</p>
-        </div>
+        <a href="">
+            <div class="card">
+                <h1>Email</h1>
+                <img src="assets/img/home-email.png" alt="">
+                <p>soporte@krear3d.com</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="card">
+                <h1>WhatsApp</h1>
+                <img src="assets/img/whatsapp.png" alt="">
+                <p>+51 970 539 751</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="card">
+                <h1>Encuéntranos</h1>
+                <img src="assets/img/home-ubi.png" alt="">
+                <p>Calle Tutumo 116, Surco</p>
+            </div>
+        </a>
         <div class="card">
             <h1>Horario</h1>
             <img src="assets/img/calendario.png" alt="">
