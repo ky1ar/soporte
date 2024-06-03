07dac649f3e4e00e7f9b7f6715d5fa1efd753ca5
alex <alex@gmail.com>
2024-06-03 15:33:30 -0500

    test

--- contacto.php
+++ contacto.php
@@ -13,24 +13,26 @@ require_once 'includes/common/header.php';
     <section class="xbanner-top">
         <img src="assets/img/banner-contacto-pc.webp">
     </section>
-    <section class="home-contact">
-        <h1>CONTÁCTANOS</h1>
-        <div class="container-cards">
-            <div class="card-info">
-                <h1>Email</h1>
-                <img src="assets/img/home-email.png" alt="">
-                <p>soporte@krear3d.com</p>
-            </div>
-            <div class="card-info">
-                <h1>WhatsApp</h1>
-                <img src="assets/img/whatsapp.png" alt="">
-                <p>+51 970 539 751</p>
-            </div>
-            <div class="card-info">
-                <h1>Encuéntranos</h1>
-                <img src="assets/img/home-ubi.png" alt="">
-                <p>Calle Tutumo 116, Surco</p>
-            </div>
+    <section class="contact-info">
+        <div class="card">
+            <h1>Email</h1>
+            <img src="assets/img/home-email.png" alt="">
+            <p>soporte@krear3d.com</p>
+        </div>
+        <div class="card">
+            <h1>WhatsApp</h1>
+            <img src="assets/img/whatsapp.png" alt="">
+            <p>+51 970 539 751</p>
+        </div>
+        <div class="card">
+            <h1>Encuéntranos</h1>
+            <img src="ssets/img/home-ubi.png" alt="">
+            <p>Calle Tutumo 116, Surco</p>
+        </div>
+        <div class="card">
+            <h1>Horario</h1>
+            <img src="ssets/img/calendar.png" alt="">
+            <p>Lun - Sab de 9:00 a 1:00 pm <br>Lun - Vie de 2:00 a 6:00 pm</p>
         </div>
     </section>
     <?php require_once 'includes/common/footer.php'; ?>
