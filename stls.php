a2de9b2c6a8f60882179d45241e7314f535d5dd7
alex <alex@gmail.com>
2024-06-04 12:33:50 -0500

    test

---
+++ stls.php
@@ -0,0 +1,19 @@
+<?php
+$currentPage = "Contacto";
+require_once 'includes/app/globals.php';
+require_once 'includes/common/header.php';
+?>
+</head>
+
+<body>
+    <?php
+    require_once 'includes/bar/topBar.php';
+    require_once 'includes/bar/navigationBar.php';
+    ?>
+
+    <section class="xbanner-top">
+        <img src="assets/img/stl-pc.webp">
+    </section>
+</body>
+
+</html>
