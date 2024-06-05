fd3b93b3ac41785e08b1dc501f6077026d352477
alex <alex@gmail.com>
2024-06-05 14:26:00 -0500

    test

--- wiki.php
+++ wiki.php
@@ -14,39 +14,71 @@ require_once 'includes/common/header.php';
     <section class="xbanner-wiki">
         <img src="assets/img/stl-pc.webp">
     </section>
-    <section class="">
+    <section class="menu-wiki">
         <h1>Inicio</h1>
         <ul>
             <li>
-                <a href="#">SOLUCION DE PROBLEMAS</a>
+                <a href="#">INICIO</a>
+            </li>
+            <li>
+                <a href="#">SOLUCIÓN DE PROBLEMAS</a>
                 <ul>
-                    <li><a href="#">Subopción 3.1</a></li>
-                    <li><a href="#">Subopción 3.2</a></li>
-                    <li><a href="#">Subopción 3.3</a></li>
+                    <li><a href="#">RESIDUO EN EL EXTRUSOR</a></li>
+                    <li><a href="#">PROBLEMAS CON EL RELLENO</a></li>
+                    <li><a href="#">SOPORTES QUE FALLAN</a></li>
+                    <li><a href="#">SUPERFICIES EN CONTACTO CON SOPORTES CON MAL ASPECTO</a></li>
+                    <li><a href="#">GHOSTING</a></li>
+                    <li><a href="#">SEPARACIÓN DE CAPAS</a></li>
+                    <li><a href="#">PUENTES FEOS</a></li>
+                    <li><a href="#">WARPING</a></li>
+                    <li><a href="#">SUBEXTRUSIÓN</a></li>
+                    <li><a href="#">TEST DEL BENCHY</a></li>
+                    <li><a href="#">HILOS Y GOTEO</a></li>
+                    <li><a href="#">PREVENCIÓN DE PROBLEMAS DE EXTRUSIÓN</a></li>
+                    <li><a href="#">DESPLAZAMIENTO DE CAPAS</a></li>
+                    <li><a href="#">PROBLEMAS EN LA PRIMERA CAPA</a></li>
+                    <li><a href="#">LA IMPRESORA NO ENCIENDE O SIGUE APAGÁNDOSE</a></li>
+                    <li><a href="#">LA PLATAFORMA DE IMPRESIÓN NO SE ESTÁ CALENTANDO CORRECTAMENTE</a></li>
+                    <li><a href="#">RUIDOS EN EL EXTRUSOR</a></li>
+                    <li><a href="#">EL FILAMENTO NO SE CARGA</a></li>
+                    <li><a href="#">USO DEL MULTÍMETRO</a></li>
+                    <li><a href="#">LA EXTRUSIÓN SE DETUVO A LA MITAD DE LA IMPRESIÓN</a></li>
+                    <li><a href="#">BOQUILLA / HOTEND OBSTRUIDOS</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#">USANDO LA IMPRESORA</a>
                 <ul>
-                    <li><a href="#">Subopción 3.1</a></li>
-                    <li><a href="#">Subopción 3.2</a></li>
-                    <li><a href="#">Subopción 3.3</a></li>
+                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE</a></li>
+                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA</a></li>
+                    <li><a href="#">MANTENIMIENTO HABITUAL</a></li>
+                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - ANYCUBIC</a></li>
+                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - ARTILLERY</a></li>
+                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - CREALITY</a></li>
+                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - ARTILLERY</a></li>
+                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - ANYCUBIC</a></li>
+                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - CREALITY</a></li>
+                    <li><a href="#">MATERIALES FLEXIBLES</a></li>
+                    <li><a href="#">PETG</a></li>
+                    <li><a href="#">PLA</a></li>
+                    <li><a href="#">MODELOS 3D CON ERRORES</a></li>
+                    <li><a href="#">CREANDO TUS PROPIOS MODELOS 3D</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#">CALIBRACIÓN</a>
                 <ul>
-                    <li><a href="#">Subopción 3.1</a></li>
-                    <li><a href="#">Subopción 3.2</a></li>
-                    <li><a href="#">Subopción 3.3</a></li>
+                    <li><a href="#">NIVELACIÓN DE LA CAMA</a></li>
+                    <li><a href="#">CORRECCIÓN DE LA NIVELACIÓN DE LA CAMA</a></li>
+                    <li><a href="#">CALIBRACIÓN DEL FLUJO</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#">SOBRE TU IMPRESORA</a>
                 <ul>
-                    <li><a href="#">Subopción 3.1</a></li>
-                    <li><a href="#">Subopción 3.2</a></li>
-                    <li><a href="#">Subopción 3.3</a></li>
+                    <li><a href="#">DOCUMENTACIÓN CON FOTOS Y VIDEOS</a></li>
+                    <li><a href="#">PROBLEMAS CON TUS TARJETAS SD Y UNIDADES USB</a></li>
+                    <li><a href="#">MANUALES DE MONTAJE</a></li>
                 </ul>
             </li>
         </ul>
