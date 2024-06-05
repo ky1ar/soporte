c081d5702cd53edd4c0299923beab4313e1b2e64
alex <alex@gmail.com>
2024-06-05 14:20:35 -0500

    test

--- wiki.php
+++ wiki.php
@@ -14,6 +14,43 @@ require_once 'includes/common/header.php';
     <section class="xbanner-wiki">
         <img src="assets/img/stl-pc.webp">
     </section>
+    <section class="">
+        <h1>Inicio</h1>
+        <ul>
+            <li>
+                <a href="#">SOLUCION DE PROBLEMAS</a>
+                <ul>
+                    <li><a href="#">Subopción 3.1</a></li>
+                    <li><a href="#">Subopción 3.2</a></li>
+                    <li><a href="#">Subopción 3.3</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">USANDO LA IMPRESORA</a>
+                <ul>
+                    <li><a href="#">Subopción 3.1</a></li>
+                    <li><a href="#">Subopción 3.2</a></li>
+                    <li><a href="#">Subopción 3.3</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">CALIBRACIÓN</a>
+                <ul>
+                    <li><a href="#">Subopción 3.1</a></li>
+                    <li><a href="#">Subopción 3.2</a></li>
+                    <li><a href="#">Subopción 3.3</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">SOBRE TU IMPRESORA</a>
+                <ul>
+                    <li><a href="#">Subopción 3.1</a></li>
+                    <li><a href="#">Subopción 3.2</a></li>
+                    <li><a href="#">Subopción 3.3</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
 
     <?php require_once 'includes/common/footer.php'; ?>
 </body>
