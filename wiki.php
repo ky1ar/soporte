b15a7250451f84a1bf8cc08ee7da634dd3ecf382
alex <alex@gmail.com>
2024-06-05 14:31:44 -0500

    test

--- wiki.php
+++ wiki.php
@@ -22,62 +22,66 @@ require_once 'includes/common/header.php';
             <li>
                 <a href="#">SOLUCIÓN DE PROBLEMAS</a>
                 <ul>
-                    <li><a href="#">RESIDUO EN EL EXTRUSOR</a></li>
-                    <li><a href="#">PROBLEMAS CON EL RELLENO</a></li>
-                    <li><a href="#">SOPORTES QUE FALLAN</a></li>
-                    <li><a href="#">SUPERFICIES EN CONTACTO CON SOPORTES CON MAL ASPECTO</a></li>
-                    <li><a href="#">GHOSTING</a></li>
-                    <li><a href="#">SEPARACIÓN DE CAPAS</a></li>
-                    <li><a href="#">PUENTES FEOS</a></li>
-                    <li><a href="#">WARPING</a></li>
-                    <li><a href="#">SUBEXTRUSIÓN</a></li>
-                    <li><a href="#">TEST DEL BENCHY</a></li>
-                    <li><a href="#">HILOS Y GOTEO</a></li>
-                    <li><a href="#">PREVENCIÓN DE PROBLEMAS DE EXTRUSIÓN</a></li>
-                    <li><a href="#">DESPLAZAMIENTO DE CAPAS</a></li>
-                    <li><a href="#">PROBLEMAS EN LA PRIMERA CAPA</a></li>
-                    <li><a href="#">LA IMPRESORA NO ENCIENDE O SIGUE APAGÁNDOSE</a></li>
-                    <li><a href="#">LA PLATAFORMA DE IMPRESIÓN NO SE ESTÁ CALENTANDO CORRECTAMENTE</a></li>
-                    <li><a href="#">RUIDOS EN EL EXTRUSOR</a></li>
-                    <li><a href="#">EL FILAMENTO NO SE CARGA</a></li>
-                    <li><a href="#">USO DEL MULTÍMETRO</a></li>
-                    <li><a href="#">LA EXTRUSIÓN SE DETUVO A LA MITAD DE LA IMPRESIÓN</a></li>
-                    <li><a href="#">BOQUILLA / HOTEND OBSTRUIDOS</a></li>
+                    <li><a href="#">Residuos En El Extrusor</a></li>
+                    <li><a href="#">Problemas Con El Relleno</a></li>
+                    <li><a href="#">Soportes Que Fallan</a></li>
+                    <li><a href="#">Superficies En Contacto Con Soportes Con Mal Aspecto</a></li>
+                    <li><a href="#">Ghosting</a></li>
+                    <li><a href="#">Separación De Capas</a></li>
+                    <li><a href="#">Puentes Feos</a></li>
+                    <li><a href="#">Warping</a></li>
+                    <li><a href="#">Subextrusión</a></li>
+                    <li><a href="#">Test Del Benchy</a></li>
+                    <li><a href="#">Hilos Y Goteo</a></li>
+                    <li><a href="#">Prevención De Problemas De Extrusión</a></li>
+                    <li><a href="#">Desplazamiento De Capas</a></li>
+                    <li><a href="#">Problemas En La Primera Capa</a></li>
+                    <li><a href="#">La Impresora No Enciende O Sigue Apagándose</a></li>
+                    <li><a href="#">La Plataforma De Impresión No Se Está Calentando Correctamente</a></li>
+                    <li><a href="#">Ruidos En El Extrusor</a></li>
+                    <li><a href="#">El Filamento No Se Carga</a></li>
+                    <li><a href="#">Uso Del Multímetro</a></li>
+                    <li><a href="#">La Extrusión Se Detuvo A La Mitad De La Impresión</a></li>
+                    <li><a href="#">Boquilla / Hotend Obstruidos</a></li>
+
                 </ul>
             </li>
             <li>
                 <a href="#">USANDO LA IMPRESORA</a>
                 <ul>
-                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE</a></li>
-                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA</a></li>
-                    <li><a href="#">MANTENIMIENTO HABITUAL</a></li>
-                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - ANYCUBIC</a></li>
-                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - ARTILLERY</a></li>
-                    <li><a href="#">CAMBIAR O REEMPLAZAR LA BOQUILLA - CREALITY</a></li>
-                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - ARTILLERY</a></li>
-                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - ANYCUBIC</a></li>
-                    <li><a href="#">ACTUALIZACIÓN DEL FIRMWARE - CREALITY</a></li>
-                    <li><a href="#">MATERIALES FLEXIBLES</a></li>
+                    <li><a href="#">Actualización Del Firmware</a></li>
+                    <li><a href="#">Cambiar O Reemplazar La Boquilla</a></li>
+                    <li><a href="#">Mantenimiento Habitual</a></li>
+                    <li><a href="#">Cambiar O Reemplazar La Boquilla - Anycubic</a></li>
+                    <li><a href="#">Cambiar O Reemplazar La Boquilla - Artillery</a></li>
+                    <li><a href="#">Cambiar O Reemplazar La Boquilla - Creality</a></li>
+                    <li><a href="#">Actualización Del Firmware - Artillery</a></li>
+                    <li><a href="#">Actualización Del Firmware - Anycubic</a></li>
+                    <li><a href="#">Actualización Del Firmware - Creality</a></li>
+                    <li><a href="#">Materiales Flexibles</a></li>
                     <li><a href="#">PETG</a></li>
                     <li><a href="#">PLA</a></li>
-                    <li><a href="#">MODELOS 3D CON ERRORES</a></li>
-                    <li><a href="#">CREANDO TUS PROPIOS MODELOS 3D</a></li>
+                    <li><a href="#">Modelos 3D Con Errores</a></li>
+                    <li><a href="#">Creando Tus Propios Modelos 3D</a></li>
+
                 </ul>
             </li>
             <li>
                 <a href="#">CALIBRACIÓN</a>
                 <ul>
-                    <li><a href="#">NIVELACIÓN DE LA CAMA</a></li>
-                    <li><a href="#">CORRECCIÓN DE LA NIVELACIÓN DE LA CAMA</a></li>
-                    <li><a href="#">CALIBRACIÓN DEL FLUJO</a></li>
+                    <li><a href="#">Nivelación De La Cama</a></li>
+                    <li><a href="#">Corrección De La Nivelación De La Cama</a></li>
+                    <li><a href="#">Calibración Del Flujo</a></li>
+
                 </ul>
             </li>
             <li>
                 <a href="#">SOBRE TU IMPRESORA</a>
                 <ul>
-                    <li><a href="#">DOCUMENTACIÓN CON FOTOS Y VIDEOS</a></li>
-                    <li><a href="#">PROBLEMAS CON TUS TARJETAS SD Y UNIDADES USB</a></li>
-                    <li><a href="#">MANUALES DE MONTAJE</a></li>
+                    <li><a href="#">Documentación Con Fotos Y Videos</a></li>
+                    <li><a href="#">Problemas Con Tus Tarjetas SD Y Unidades USB</a></li>
+                    <li><a href="#">Manuales De Montaje</a></li>
+
                 </ul>
             </li>
         </ul>
