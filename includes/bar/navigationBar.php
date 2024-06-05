e563c3655c775a3f64d4e17a952c57c6b31c3b41
alex <alex@gmail.com>
2024-06-05 11:01:11 -0500

    test

--- includes/bar/navigationBar.php
+++ includes/bar/navigationBar.php
@@ -3,7 +3,7 @@ $pages = [
     // ['Home','Home'],
     ['Consultas', 'consultas'],
     ['Capacitaciones', 'capacitaciones'],
-    ['STLs', 'stls'],
+    ['STLs', 'stls.php'],
     ['Contacto', 'contacto'],
     // ['Conocimiento', 'conocimiento'],
     // ['Cursos', 'cursos']
