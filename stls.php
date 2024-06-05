547cda66041e486c6aadedd025db4b35b5414327
alex <alex@gmail.com>
2024-06-05 10:18:41 -0500

    test

--- stls.php
+++ stls.php
@@ -24,7 +24,50 @@ require_once 'includes/common/header.php';
                 <button>DESCARGA AQUÍ</button>
             </a>
         </div>
+        <div class="card-stl">
+            <img src="assets/img/pika.png" alt="">
+            <h1>OBJETO1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <a href="stls/pencil-stl.zip" download>
+                <button>DESCARGA AQUÍ</button>
+            </a>
+        </div>
+        <div class="card-stl">
+            <img src="assets/img/pika.png" alt="">
+            <h1>OBJETO1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <a href="stls/pencil-stl.zip" download>
+                <button>DESCARGA AQUÍ</button>
+            </a>
+        </div>
     </section>
+    <section class="stls">
+        <div class="card-stl">
+            <img src="assets/img/pika.png" alt="">
+            <h1>OBJETO1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <a href="stls/pencil-stl.zip" download>
+                <button>DESCARGA AQUÍ</button>
+            </a>
+        </div>
+        <div class="card-stl">
+            <img src="assets/img/pika.png" alt="">
+            <h1>OBJETO1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <a href="stls/pencil-stl.zip" download>
+                <button>DESCARGA AQUÍ</button>
+            </a>
+        </div>
+        <div class="card-stl">
+            <img src="assets/img/pika.png" alt="">
+            <h1>OBJETO1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <a href="stls/pencil-stl.zip" download>
+                <button>DESCARGA AQUÍ</button>
+            </a>
+        </div>
+    </section>
+
     <?php require_once 'includes/common/footer.php'; ?>
 </body>
 
