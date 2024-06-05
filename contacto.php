8a29a49dee361daf9c46d30d1a93f1c2a7c58319
alex <alex@gmail.com>
2024-06-05 09:59:32 -0500

    test

--- contacto.php
+++ contacto.php
@@ -48,8 +48,8 @@ require_once 'includes/common/header.php';
     <section class="contact-map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.727233699139!2d-77.00177192418383!3d-12.13080614347271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b807d9f38e5b%3A0xc35c73ba4479f1ea!2sC.%20Tutumo%20116%2C%20Santiago%20de%20Surco%2015038!5e0!3m2!1ses-419!2spe!4v1717449253128!5m2!1ses-419!2spe" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <div>
-            <h1>Contactanos</h1>
-            <p>Contactanos con diversos canales de comunicación para ayudar a resolver tus dudas y consultas.</p>
+            <h1>Contáctanos</h1>
+            <p>Contáctanos desde nuestros distintos canales de comunicación para ayudarte a resolver todas tus dudas y consultas.</p>
         </div>
     </section>
     <?php require_once 'includes/common/footer.php'; ?>
