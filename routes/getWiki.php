fd638f0103fdfb774a80098a4cd1b3cc3eb32b01
alex <alex@gmail.com>
2024-06-10 14:53:15 -0500

    test

--- routes/getWiki.php
+++ routes/getWiki.php
@@ -22,21 +22,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
 
         if ($result->num_rows > 0) {
             $data = $result->fetch_assoc();
-            
+
             // Reemplazar \r\n y \n por <br> en los valores de los textos
             $textKeys = [
-                'p1', 'coment1', 'pex1', 'p2', 'coment2', 'p3', 'pex3', 'p4', 'p5', 
-                'p51', 'p52', 'p6', 'p61', 'p7', 'p71', 'p72', 'p73', 'p74', 'p8', 
-                'p81', 'p82', 'p811', 'p821', 'p831', 'p841', 'p851', 'p861', 'p822', 
-                'p823', 'p824', 'p825', 'p826', 'p8261', 'p9'
+                'p1', 'coment1', 'pex1', 'p2', 'coment2', 'p3', 'pex3', 'p4', 'p5',
+                'p51', 'p52', 'p6', 'p61', 'p7', 'p71', 'p72', 'p73', 'p74', 'p8',
+                'p81', 'p82', 'p811', 'p821', 'p831', 'p841', 'p851', 'p861', 'p822',
+                'p823', 'p824', 'p825', 'p826', 'p8261', 'p9', 'p721', 'p731', 'p711'
             ];
-            
+
             foreach ($textKeys as $key) {
                 if (isset($data[$key])) {
                     $data[$key] = str_replace(array("\r\n", "\n"), "<br>", $data[$key]);
                 }
             }
-            
+
             $response['success'] = true;
             $response['data'] = $data;
         } else {
@@ -57,4 +57,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
 }
 
 echo json_encode($response);
-?>
