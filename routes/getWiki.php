3a59fa819a75a20901ad9581fe5a934ac73d4f3d
alex <alex@gmail.com>
2024-06-10 14:46:23 -0500

    test

--- routes/getWiki.php
+++ routes/getWiki.php
@@ -24,7 +24,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
             $data = $result->fetch_assoc();
             
             // Reemplazar \n por <br> en los valores de los textos
-            $textKeys = ['p1', 'coment1', 'pex1', 'p2', 'coment2', 'p3', 'pex3', 'p4', 'p5', 'p51', 'p52', 'p6', 'p61', 'p7', 'p71', 'p72', 'p73', 'p74', 'p8', 'p81', 'p82', 'p811', 'p821', 'p831', 'p841', 'p851', 'p861', 'p822', 'p823', 'p824', 'p825', 'p826', 'p8261', 'p9'];
+            $textKeys = [
+                'p1', 'coment1', 'pex1', 'p2', 'coment2', 'p3', 'pex3', 'p4', 'p5', 
+                'p51', 'p52', 'p6', 'p61', 'p7', 'p71', 'p72', 'p73', 'p74', 'p8', 
+                'p81', 'p82', 'p811', 'p821', 'p831', 'p841', 'p851', 'p861', 'p822', 
+                'p823', 'p824', 'p825', 'p826', 'p8261', 'p9'
+            ];
+            
             foreach ($textKeys as $key) {
                 if (isset($data[$key])) {
                     $data[$key] = str_replace("\n", "<br>", $data[$key]);
