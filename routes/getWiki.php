be5ef1ce4bdc11b47d855a6531b8836f5afd6377
alex <alex@gmail.com>
2024-06-11 14:32:17 -0500

    test

--- routes/getWiki.php
+++ routes/getWiki.php
@@ -23,35 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
         if ($result->num_rows > 0) {
             $data = $result->fetch_assoc();
 
-            // Función para aplicar estilos directamente al texto
-            function aplicarEstilosTexto($texto) {
-                $lineas = explode("\n", $texto);
-                $texto_formateado = "";
-
-                foreach ($lineas as $linea) {
-                    $linea_trim = trim($linea);
-                    
-                    if (preg_match('/^\d+\./', $linea_trim) || strpos($linea_trim, '•') === 0) {
-                        // Buscar el primer ':' después del número y punto o viñeta
-                        $pos_dos_puntos = strpos($linea_trim, ':');
-                        if ($pos_dos_puntos !== false) {
-                            $parte_inicial = substr($linea_trim, 0, $pos_dos_puntos + 1);
-                            $parte_restante = substr($linea_trim, $pos_dos_puntos + 1);
-                            $linea_formateada = '<span style="font-weight: 600;">' . htmlspecialchars($parte_inicial) . '</span>' . htmlspecialchars($parte_restante);
-                        } else {
-                            $linea_formateada = htmlspecialchars($linea_trim);
-                        }
-                        // Aplicar estilos directamente al texto dentro de las etiquetas <p>
-                        $texto_formateado .= '<p style="padding-left: 1rem; text-indent: -1rem; margin: 0;">' . $linea_formateada . '</p>';
-                    } else {
-                        $texto_formateado .= '<p style="margin: 0;">' . htmlspecialchars($linea_trim) . '</p>';
-                    }
-                }
-
-                return $texto_formateado;
-            }
-
-            // Aplicar la función a los datos
+            // Reemplazar \r\n y \n por <br> en los valores de los textos
             $textKeys = [
                 'p1', 'coment1', 'pex1', 'p2', 'coment2', 'p3', 'pex3', 'p4', 'p5',
                 'p51', 'p52', 'p6', 'p61', 'p7', 'p71', 'p72', 'p73', 'p74', 'p8',
@@ -61,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
 
             foreach ($textKeys as $key) {
                 if (isset($data[$key])) {
-                    $data[$key] = aplicarEstilosTexto($data[$key]);
+                    $data[$key] = str_replace(array("\r\n", "\n"), "<br>", $data[$key]);
                 }
             }
 
@@ -85,4 +57,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['title'])) {
 }
 
 echo json_encode($response);
-?>
