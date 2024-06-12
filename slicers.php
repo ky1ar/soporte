9d2973152060cffe817321fb19640f4be9baed5e
alex <alex@gmail.com>
2024-06-12 12:39:18 -0500

    test

--- slicers.php
+++ slicers.php
@@ -84,6 +84,144 @@ require_once 'includes/common/header.php';
             </div>
 
         </div>
+
+        <div class="cont-cards">
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-cura.png" alt="">
+                </div>
+                <div>
+                    <h1>UltiMaker Cura</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://ultimaker.com/es/software/ultimaker-cura/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-prusa.png" alt="">
+                </div>
+                <div>
+                    <h1>PrusaSlicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.prusa3d.com/es/pagina/prusaslicer_424/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-slic3r.png" alt="">
+                </div>
+                <div>
+                    <h1>Slic3r</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://slic3r.org/download/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-chitubox.png" alt="">
+                </div>
+                <div>
+                    <h1>Chitubox</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.chitubox.com/en/download/chitubox-free" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-mattercontrol.png" alt="">
+                </div>
+                <div>
+                    <h1>MatterControl</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.matterhackers.com/store/l/mattercontrol/sk/MKZGTDW6" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-orca.png" alt="">
+                </div>
+                <div>
+                    <h1>Orca Slicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://github.com/SoftFever/OrcaSlicer/releases/tag/v2.0.0" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="cont-cards">
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-cura.png" alt="">
+                </div>
+                <div>
+                    <h1>UltiMaker Cura</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://ultimaker.com/es/software/ultimaker-cura/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-prusa.png" alt="">
+                </div>
+                <div>
+                    <h1>PrusaSlicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.prusa3d.com/es/pagina/prusaslicer_424/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-slic3r.png" alt="">
+                </div>
+                <div>
+                    <h1>Slic3r</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://slic3r.org/download/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-chitubox.png" alt="">
+                </div>
+                <div>
+                    <h1>Chitubox</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.chitubox.com/en/download/chitubox-free" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-mattercontrol.png" alt="">
+                </div>
+                <div>
+                    <h1>MatterControl</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.matterhackers.com/store/l/mattercontrol/sk/MKZGTDW6" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-orca.png" alt="">
+                </div>
+                <div>
+                    <h1>Orca Slicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://github.com/SoftFever/OrcaSlicer/releases/tag/v2.0.0" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+        </div>
     </section>
     <section class="cortlas"></section>
     <section class="es3d"></section>
