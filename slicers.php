a36732df47fe647077af3f78d1f313193a520193
alex <alex@gmail.com>
2024-06-12 12:43:10 -0500

    test

--- slicers.php
+++ slicers.php
@@ -89,7 +89,7 @@ require_once 'includes/common/header.php';
     </section>
 
     <section class="i3d">
-        <h1 class="sub">IMPRESORAS 3D</h1>
+        <h1 class="sub">CORTADORAS LASERS</h1>
         <div class="cont-cards">
             <div class="card">
                 <div>
@@ -163,7 +163,7 @@ require_once 'includes/common/header.php';
     </section>
 
     <section class="i3d">
-        <h1 class="sub">IMPRESORAS 3D</h1>
+        <h1 class="sub">ESCANERES 3D</h1>
         <div class="cont-cards">
             <div class="card">
                 <div>
@@ -233,10 +233,78 @@ require_once 'includes/common/header.php';
 
         </div>
     </section>
-    
-    <section class="cortlas"></section>
-    <section class="es3d"></section>
-    <section class="cnc"></section>
+
+    <section class="i3d">
+        <h1 class="sub">CNC</h1>
+        <div class="cont-cards">
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-cura.png" alt="">
+                </div>
+                <div>
+                    <h1>UltiMaker Cura</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://ultimaker.com/es/software/ultimaker-cura/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-prusa.png" alt="">
+                </div>
+                <div>
+                    <h1>PrusaSlicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.prusa3d.com/es/pagina/prusaslicer_424/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-slic3r.png" alt="">
+                </div>
+                <div>
+                    <h1>Slic3r</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://slic3r.org/download/" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-chitubox.png" alt="">
+                </div>
+                <div>
+                    <h1>Chitubox</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.chitubox.com/en/download/chitubox-free" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-mattercontrol.png" alt="">
+                </div>
+                <div>
+                    <h1>MatterControl</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://www.matterhackers.com/store/l/mattercontrol/sk/MKZGTDW6" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="assets/img/logos-slicer/logo-orca.png" alt="">
+                </div>
+                <div>
+                    <h1>Orca Slicer</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
+                    <a href="https://github.com/SoftFever/OrcaSlicer/releases/tag/v2.0.0" target="_blank">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+
+        </div>
+    </section>
     <?php require_once 'includes/common/footer.php'; ?>
 
 </body>
