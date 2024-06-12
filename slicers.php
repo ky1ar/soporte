04013afc17f6b904ba94b8c8c40d4cd620a94794
alex <alex@gmail.com>
2024-06-12 10:57:35 -0500

    test

--- slicers.php
+++ slicers.php
@@ -13,6 +13,23 @@ require_once 'includes/common/header.php';
     <section class="xbanner-slicers">
         <img src="assets/img/SLICERS-pc.webp" alt="">
     </section>
+    <h1>SLICERS</h1>
+    <section class="i3d">
+        <h1>IMPRESORAS 3D</h1>
+        <div class="cont-cards">
+            <div class="card-s">
+                <img src="assets/img/logos-slicer/logo-cura.webp" alt="">
+                <div>
+                    <h1>UltiMaker Cura</h1>
+                    <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas. Ofrece más de 400 ajustes para optimizar tus modelos 3D y obtener los mejores resultados de impresión.</p>
+                    <a href="https://ultimaker.com/es/software/ultimaker-cura/">DESCARGA AQUÍ</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="cortlas"></section>
+    <section class="es3d"></section>
+    <section class="cnc"></section>
     <?php require_once 'includes/common/footer.php'; ?>
 
 </body>
