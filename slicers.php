c0beaf564f9817000ab606478c0372ae7ad9d4f5
alex <alex@gmail.com>
2024-06-12 15:03:29 -0500

    test

--- slicers.php
+++ slicers.php
@@ -165,34 +165,34 @@ require_once 'includes/common/header.php';
         <div class="cont-cards">
             <div class="card">
                 <div>
-                    <img src="assets/img/logos-slicer/logo-cura.png" alt="">
+                    <img src="assets/img/logos-slicer/logo-meshlab.png" alt="">
                 </div>
                 <div>
-                    <h1>UltiMaker Cura</h1>
+                    <h1>Meshlab</h1>
                     <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
-                    <a href="https://ultimaker.com/es/software/ultimaker-cura/" target="_blank">DESCARGA AQUÍ</a>
+                    <a href="https://www.meshlab.net/" target="_blank">DESCARGA AQUÍ</a>
                 </div>
             </div>
 
             <div class="card">
                 <div>
-                    <img src="assets/img/logos-slicer/logo-prusa.png" alt="">
+                    <img src="assets/img/logos-slicer/logo-cc.png" alt="">
                 </div>
                 <div>
-                    <h1>PrusaSlicer</h1>
+                    <h1>CloudCompare</h1>
                     <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
-                    <a href="https://www.prusa3d.com/es/pagina/prusaslicer_424/" target="_blank">DESCARGA AQUÍ</a>
+                    <a href="https://cloudcompare-org.danielgm.net/release/" target="_blank">DESCARGA AQUÍ</a>
                 </div>
             </div>
 
             <div class="card">
                 <div>
-                    <img src="assets/img/logos-slicer/logo-slic3r.png" alt="">
+                    <img src="assets/img/logos-slicer/logo-regard3d.jpg" alt="">
                 </div>
                 <div>
-                    <h1>Slic3r</h1>
+                    <h1>Regard3D</h1>
                     <p>Cura de Ultimaker es un software de impresión 3D gratuito y popular, utilizado por millones de personas.</p>
-                    <a href="https://slic3r.org/download/" target="_blank">DESCARGA AQUÍ</a>
+                    <a href="https://www.regard3d.org/index.php/download" target="_blank">DESCARGA AQUÍ</a>
                 </div>
             </div>
 
