7b153e634aa8ae2cb2b02f7ea1469a307d92210f
alex <alex@gmail.com>
2024-06-13 12:33:45 -0500

    test

--- stl.php
+++ stl.php
@@ -19,7 +19,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/pika.webp" alt="">
             <h1>PIKACHU</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Modelo STL de Pikachu, ideal para impresión en 3D. Captura la esencia del personaje con este diseño detallado.</p>
             <a href="archivos-stl/pikachu.rar" download>
                 <button>DESCARGAR</button>
             </a>
@@ -27,7 +27,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/cajas.webp" alt="">
             <h1>PUZZLE</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Descarga este archivo STL de puzzle para crear tu propio rompecabezas personalizado en 3D. Perfecto para entusiastas del diseño y los juegos.</p>
             <a href="archivos-stl/puzzle-stl.zip" download>
                 <button>DESCARGAR</button>
             </a>
@@ -35,7 +35,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/dragon.webp" alt="">
             <h1>DRAGON</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Explora este modelo STL de un dragón, listo para ser impreso en 3D. Detalles impresionantes que hacen que este diseño sea único.</p>
             <a href="archivos-stl/dragon-stl.zip" download>
                 <button>DESCARGAR</button>
             </a>
@@ -45,7 +45,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/egg.webp" alt="">
             <h1>HUEVOS DE PASCUA</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Archivos STL de huevos de Pascua, ideales para la temporada. Crea decoraciones personalizadas con este diseño versátil.</p>
             <a href="archivos-stl/egg-stl.rar" download>
                 <button>DESCARGAR</button>
             </a>
@@ -53,7 +53,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/knight.webp" alt="">
             <h1>KNIGHT</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Impresiona con este modelo STL de un caballero. Perfecto para aficionados a la fantasía y coleccionistas de figuras en 3D.</p>
             <a href="archivos-stl/knight-stl.rar" download>
                 <button>DESCARGAR</button>
             </a>
@@ -61,7 +61,7 @@ require_once 'includes/common/header.php';
         <div class="card-stl">
             <img src="assets/img/naruto.webp" alt="">
             <h1>NARUTO</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos asperiores eligendi consequuntur soluta itaque fugiat debitis in esse odit.</p>
+            <p>Revive tus momentos favoritos de Naruto con este archivo STL. Perfecto para fans que desean agregar un toque personal a su colección.</p>
             <a href="archivos-stl/naruto-stl.zip" download>
                 <button>DESCARGAR</button>
             </a>
