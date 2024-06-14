72d39a1c69d4f6f52b8ba67db6d9373259e77eff
alex <alex@gmail.com>
2024-06-14 17:25:26 -0500

    test

--- stl.php
+++ stl.php
@@ -18,10 +18,10 @@ require_once 'includes/common/header.php';
     <!-- Contenedor para las tarjetas de STLs -->
     <div id="stls-container"></div>
     <!-- Controles de paginación -->
-    <div class="page-">
-        <button id="prevPage">Anterior</button>
+    <div class="page-stls">
+        <button id="prevPage"><img src="assets/img/next-page.png" alt=""></button>
         <span id="pageIndicator"></span>
-        <button id="nextPage">Siguiente</button>
+        <button id="nextPage"><img src="assets/img/next-page.png" alt=""></button>
     </div>
     <?php require_once 'includes/common/footer.php'; ?>
 </body>
