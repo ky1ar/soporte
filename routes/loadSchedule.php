a9d10efd0e4b269dcccfa8eae2979b1f29cfa41c
alex <alex@gmail.com>
2024-08-23 12:27:03 -0500

    test

--- routes/loadSchedule.php
+++ routes/loadSchedule.php
@@ -23,27 +23,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['date'])) {
         $response['html'] .= '<ul>';
 
         if ($custom == 1) {
-            $sql2 = "SELECT cs.id, cs.h_start, cs.h_end FROM Custom_Schedule cs LEFT JOIN Training t ON t.training_date = cs.t_date WHERE cs.t_date = '$date' AND COALESCE(t.training_start, -1) != cs.h_start ORDER BY cs.h_start;";
+            // Para los horarios personalizados, verificamos si están asignados a una sesión de entrenamiento
+            $sql2 = "SELECT cs.id, cs.h_start, cs.h_end 
+                     FROM Custom_Schedule cs
+                     LEFT JOIN Training t ON cs.h_start = t.training_start AND cs.t_date = t.training_date
+                     WHERE cs.t_date = '$date' 
+                     AND (t.id IS NULL) 
+                     ORDER BY cs.h_start;";
             $result2 = $conn->query($sql2);
             while ($row2 = $result2->fetch_assoc()) {
                 $response['html'] .= '<li><div class="boxSchedule" data-schedule="'.substr($row2['h_start'], 0, 5).'">'.substr($row2['h_start'], 0, 5).'</div></li>';
             }
         } else {
-            $sql2 = 
-            "SELECT 
-                ds.id,
-                ds.h_start,
-                ds.h_end,
-                MIN(t.training_state) AS training_state
-            FROM Default_Schedule ds
-            LEFT JOIN Training t ON ds.h_start = t.training_start
-            AND t.training_date = '$date'
-            GROUP BY ds.id, ds.h_start, ds.h_end
-            HAVING MIN(t.training_state) != 0 
-                AND MIN(t.training_state) != 1
-                AND MIN(t.training_state) != 2
-                OR MIN(t.training_state) IS NULL ORDER BY ds.h_start;";
-
+            // Para los horarios predeterminados, filtramos según el estado de la sesión de entrenamiento
+            $sql2 = "SELECT 
+                        ds.id,
+                        ds.h_start,
+                        ds.h_end,
+                        MIN(t.training_state) AS training_state
+                     FROM Default_Schedule ds
+                     LEFT JOIN Training t ON ds.h_start = t.training_start
+                     AND t.training_date = '$date'
+                     GROUP BY ds.id, ds.h_start, ds.h_end
+                     HAVING MIN(t.training_state) != 0 
+                        AND MIN(t.training_state) != 1
+                        AND MIN(t.training_state) != 2
+                        OR MIN(t.training_state) IS NULL 
+                     ORDER BY ds.h_start;";
+                     
             $result2 = $conn->query($sql2);
 
             while ($row2 = $result2->fetch_assoc()) {
@@ -58,4 +65,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['date'])) {
 }
 $conn->close();
 ?>
-
