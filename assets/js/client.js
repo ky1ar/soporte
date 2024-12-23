94b28a20ce2e4c4f71c9f83e4bc5da4bf1976505
alex <alex@gmail.com>
2024-12-23 11:04:07 -0500

    a1

--- assets/js/client.js
+++ assets/js/client.js
@@ -548,51 +548,43 @@ $(document).ready(function () {
     let offsetYear = currentDate.getFullYear();
 
     if (offsetMonth < 0) {
-        offsetMonth = 11;
-        offsetYear--;
+      offsetMonth = 11;
+      offsetYear--;
     }
-
     if (offsetYear === today.getFullYear() && offsetMonth >= today.getMonth()) {
-        loadCalendar(-1);
-        calendarNext.removeClass("disabled");
-        $(this).addClass("disabled");
+      loadCalendar(-1);
+      calendarNext.removeClass("disabled");
+      $(this).addClass("disabled");
     } else if (offsetYear === today.getFullYear() - 1 && offsetMonth === 11) {
-        loadCalendar(-1);
-        calendarNext.removeClass("disabled");
-        $(this).addClass("disabled");
+      loadCalendar(-1);
+      calendarNext.removeClass("disabled");
+      $(this).addClass("disabled");
     }
-});
+  });
 
-calendarNext.click(function () {
+  calendarNext.click(function () {
     let offsetMonth = currentDate.getMonth() + 1;
     let offsetYear = currentDate.getFullYear();
 
     if (offsetMonth > 11) {
-        offsetMonth = 0;
-        offsetYear++;
+      offsetMonth = 0;
+      offsetYear++;
     }
-
     if (
-        (offsetYear === today.getFullYear() &&
-            offsetMonth <= today.getMonth() + 2) ||
-        (offsetYear === today.getFullYear() + 1 && offsetMonth <= 1)
+      (offsetYear === today.getFullYear() &&
+        offsetMonth <= today.getMonth() + 2) ||
+      (offsetYear === today.getFullYear() + 1 && offsetMonth <= 1)
     ) {
-        loadCalendar(1);
-        calendarPrev.removeClass("disabled");
-        if (
-            offsetYear === today.getFullYear() &&
-            offsetMonth === today.getMonth() + 2
-        ) {
-            $(this).addClass("disabled");
-        }
+      loadCalendar(1);
+      calendarPrev.removeClass("disabled");
+      if (offsetYear === today.getFullYear() + 1 && offsetMonth === 1) {
+        $(this).addClass("disabled");
+      }
     }
-
-    // Permite retroceder después de febrero
     if (offsetMonth > today.getMonth() + 1) {
-        calendarPrev.removeClass("disabled");
+      calendarPrev.removeClass("disabled");
     }
-});
-
+  });
 
   const calendarNavigation = $("#calendarNavigation");
   const calendarBackDiv = $("#calendarBackDiv");
