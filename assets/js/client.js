afe9eceeef453f9e128a362f09aa454a7c706679
alex <alex@gmail.com>
2024-12-23 10:49:50 -0500

    a1

--- assets/js/client.js
+++ assets/js/client.js
@@ -574,14 +574,14 @@ $(document).ready(function () {
 
     if (
       (offsetYear === today.getFullYear() &&
-        offsetMonth <= today.getMonth() + 1) ||
+        offsetMonth <= today.getMonth() + 2) || // Permite avanzar hasta 2 meses
       (offsetYear === today.getFullYear() + 1 && offsetMonth === 0)
     ) {
       loadCalendar(1);
       calendarPrev.removeClass("disabled");
       if (
         offsetYear === today.getFullYear() &&
-        offsetMonth === today.getMonth() + 1
+        offsetMonth === today.getMonth() + 2 // Deshabilitar después de 2 meses
       ) {
         $(this).addClass("disabled");
       }
