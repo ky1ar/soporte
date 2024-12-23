aaf12dc384069055d9a1ae6ffdf4a26e3bb9b074
alex <alex@gmail.com>
2024-12-23 11:05:09 -0500

    a1

--- assets/js/client.js
+++ assets/js/client.js
@@ -548,43 +548,56 @@ $(document).ready(function () {
     let offsetYear = currentDate.getFullYear();
 
     if (offsetMonth < 0) {
-      offsetMonth = 11;
-      offsetYear--;
+        offsetMonth = 11;
+        offsetYear--;
     }
-    if (offsetYear === today.getFullYear() && offsetMonth >= today.getMonth()) {
-      loadCalendar(-1);
-      calendarNext.removeClass("disabled");
-      $(this).addClass("disabled");
-    } else if (offsetYear === today.getFullYear() - 1 && offsetMonth === 11) {
-      loadCalendar(-1);
-      calendarNext.removeClass("disabled");
-      $(this).addClass("disabled");
-    }
-  });
 
-  calendarNext.click(function () {
+    // Permitir retroceder a enero 2025 desde febrero 2025
+    if (offsetYear === today.getFullYear() && offsetMonth >= today.getMonth()) {
+        loadCalendar(-1);
+        calendarNext.removeClass("disabled");
+        $(this).addClass("disabled");
+    } else if (offsetYear === today.getFullYear() - 1 && offsetMonth === 11) {
+        loadCalendar(-1);
+        calendarNext.removeClass("disabled");
+        $(this).addClass("disabled");
+    } else if (offsetYear === today.getFullYear() + 1 && offsetMonth === 0) {
+        // Permitir retroceder de febrero 2025 a enero 2025
+        loadCalendar(-1);
+        calendarNext.removeClass("disabled");
+        $(this).addClass("disabled");
+    }
+});
+
+calendarNext.click(function () {
     let offsetMonth = currentDate.getMonth() + 1;
     let offsetYear = currentDate.getFullYear();
 
     if (offsetMonth > 11) {
-      offsetMonth = 0;
-      offsetYear++;
+        offsetMonth = 0;
+        offsetYear++;
     }
+
+    // Lógica para avanzar hasta dos meses hacia el futuro
     if (
-      (offsetYear === today.getFullYear() &&
-        offsetMonth <= today.getMonth() + 2) ||
-      (offsetYear === today.getFullYear() + 1 && offsetMonth <= 1)
+        (offsetYear === today.getFullYear() && offsetMonth <= today.getMonth() + 2) ||
+        (offsetYear === today.getFullYear() + 1 && offsetMonth <= 1)
     ) {
-      loadCalendar(1);
-      calendarPrev.removeClass("disabled");
-      if (offsetYear === today.getFullYear() + 1 && offsetMonth === 1) {
-        $(this).addClass("disabled");
-      }
+        loadCalendar(1);
+        calendarPrev.removeClass("disabled");
+
+        // Deshabilitar el botón "next" en el mes límite (febrero de 2025)
+        if (offsetYear === today.getFullYear() + 1 && offsetMonth === 1) {
+            $(this).addClass("disabled");
+        }
     }
+
+    // Permite retroceder después de avanzar a los meses siguientes
     if (offsetMonth > today.getMonth() + 1) {
-      calendarPrev.removeClass("disabled");
+        calendarPrev.removeClass("disabled");
     }
-  });
+});
+
 
   const calendarNavigation = $("#calendarNavigation");
   const calendarBackDiv = $("#calendarBackDiv");
