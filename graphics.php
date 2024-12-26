29339e6c74ff2e92e60f886f9b33ded3ea87e213
alex <alex@gmail.com>
2024-12-26 11:49:20 -0500

    a1

--- graphics.php
+++ graphics.php
@@ -39,10 +39,10 @@ if (isset($_SESSION['user_id'])) {
         <form id="dateForm" method="POST">
             <label for="start_date">Fecha de inicio:</label>
             <input type="date" id="start_date" name="start_date" required>
-            
+
             <label for="end_date">Fecha final:</label>
             <input type="date" id="end_date" name="end_date" required>
-            
+
             <button type="button" onclick="fetchData()">Buscar</button>
         </form>
 
@@ -58,29 +58,49 @@ if (isset($_SESSION['user_id'])) {
         function fetchData() {
             var startDate = document.getElementById('start_date').value;
             var endDate = document.getElementById('end_date').value;
+            if (!startDate || !endDate) {
+                const today = new Date();
+                const yyyy = today.getFullYear();
+                let mm = today.getMonth() + 1; // El mes comienza desde 0, por lo que sumamos 1
+                let dd = today.getDate();
+
+                if (mm < 10) {
+                    mm = '0' + mm; // Aseguramos que el mes tenga dos dígitos
+                }
+                if (dd < 10) {
+                    dd = '0' + dd; // Aseguramos que el día tenga dos dígitos
+                }
+                const formattedDate = `${yyyy}-${mm}-${dd}`; // Formato yyyy-mm-dd
+                if (!startDate) {
+                    startDate = formattedDate;
+                }
+                if (!endDate) {
+                    endDate = formattedDate;
+                }
+            }
 
             var formData = new FormData();
             formData.append('start_date', startDate);
             formData.append('end_date', endDate);
-
             fetch('./routes/searchGraphics.php', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.error) {
-                    alert(data.error);
-                } else {
-                    document.getElementById('stat1Result').textContent = 'Estadísticas con stat 1: ' + data.stat1Count;
-                    document.getElementById('stat9Result').textContent = 'Estadísticas con stat 9: ' + data.stat9Count;
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        document.getElementById('stat1Result').textContent = 'Estadísticas con stat 1: ' + data.stat1Count;
+                        document.getElementById('stat9Result').textContent = 'Estadísticas con stat 9: ' + data.stat9Count;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
         }
     </script>
+
 </body>
 
 </html>
