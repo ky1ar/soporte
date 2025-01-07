9dcf9228a95189401c4a831de44b2ea1e8c6753a
alex <alex@gmail.com>
2025-01-07 11:41:05 -0500

    a1

--- routes/searchGraphics.php
+++ routes/searchGraphics.php
@@ -4,21 +4,20 @@ require_once '../includes/app/db.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['start_date']) && isset($_POST['end_date'])) {
     $startDate = $_POST['start_date'];
     $endDate = $_POST['end_date'];
-    $workerId = isset($_POST['worker_id']) ? $_POST['worker_id'] : null; 
+    $workerId = isset($_POST['worker_id']) ? $_POST['worker_id'] : null;
 
     $startDate .= ' 00:00:00';
     $endDate .= ' 23:59:59';
 
     $sql = "
-        SELECT
-            SUM(CASE WHEN os.stat = 1 THEN 1 ELSE 0 END) AS stat1,
-            SUM(CASE WHEN os.stat = 8 THEN 1 ELSE 0 END) AS stat8
-        FROM Orders_Status os
-        INNER JOIN Orders o ON os.orders = o.id
-        INNER JOIN Users u ON o.worker = u.id
-        WHERE os.dates BETWEEN ? AND ?;
-
-    ";
+            SELECT
+                SUM(CASE WHEN os.stat = 1 THEN 1 ELSE 0 END) AS stat1,
+                SUM(CASE WHEN os.stat = 8 THEN 1 ELSE 0 END) AS stat8
+            FROM Orders_Status os
+            INNER JOIN Orders o ON os.orders = o.id
+            INNER JOIN Users u ON o.worker = u.id
+            WHERE os.dates BETWEEN ? AND ?
+        ";
     $params = array($startDate, $endDate);
     if (!is_null($workerId)) {
         $sql .= " AND u.id = ?";
@@ -26,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['start_date']) && isset
     }
 
     $stmt = $conn->prepare($sql);
-    $types = str_repeat('s', count($params)); 
+    $types = str_repeat('s', count($params));
 
     if ($stmt->bind_param($types, ...$params)) {
         if ($stmt->execute()) {
@@ -57,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['start_date']) && isset
                 $trainingStmt->execute();
                 $trainingResult = $trainingStmt->get_result();
                 $trainingData = $trainingResult->fetch_assoc();
-                
+
                 $totalTrainings = $trainingData ? $trainingData['num_rows'] : 0;
 
                 echo json_encode([
@@ -87,4 +86,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['start_date']) && isset
         'error' => 'Fechas no proporcionadas'
     ]);
 }
-?>
