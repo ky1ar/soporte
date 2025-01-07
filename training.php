6045cb4f2079c8b01b34c41bc25d36d51bf0e894
alex <alex@gmail.com>
2025-01-07 13:02:28 -0500

    a1

--- training.php
+++ training.php
@@ -194,7 +194,7 @@ if (isset($_SESSION['user_id'])) {
                             <div class="pruebas">
                                 <div>
                                     <label for="comevi">Comentarios:</label>
-                                    <textarea type="text" id="comevi" name="comevi">
+                                    <textarea type="text" id="comevi" name="comevi"></textarea>
                                 </div>
                                 <div>
                                     <label for="imgevi">Archivo:</label>
