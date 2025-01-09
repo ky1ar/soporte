60bce7237c44f5b92513d860cda69125f387f0b0
alex <alex@gmail.com>
2025-01-09 16:42:05 -0500

    a1

--- graphics.php
+++ graphics.php
@@ -180,6 +180,11 @@ if (isset($_SESSION['user_id'])) {
                                 }
                             },
                             scales: {
+                                x: {
+                                    // Ajuste de la separación
+                                    barPercentage: 0.6, // Ajusta el ancho de las barras
+                                    categoryPercentage: 0.1 // Ajusta la separación entre categorías
+                                },
                                 y: {
                                     beginAtZero: true
                                 }
