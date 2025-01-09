b16cf619f41ef09b977f49d207e1048ad80801fc
alex <alex@gmail.com>
2025-01-09 17:49:33 -0500

    a1

--- graphics.php
+++ graphics.php
@@ -59,7 +59,7 @@ if (isset($_SESSION['user_id'])) {
             <input type="date" id="end_date" name="end_date" required>
             <label for="worker_id">Técnico:</label>
             <select id="worker_id" name="worker_id">
-                <option value="">Todos</option>
+                <option value="">General</option>
                 <?php foreach ($workers as $worker): ?>
                     <option value="<?php echo $worker['id']; ?>">
                         <?php echo htmlspecialchars($worker['name'], ENT_QUOTES, 'UTF-8'); ?>
@@ -68,7 +68,7 @@ if (isset($_SESSION['user_id'])) {
             </select>
             <label for="metric_select">Categoría:</label>
             <select id="metric_select" name="metric_select">
-                <option value="">Todos</option>
+                <option value="">General</option>
                 <option value="stat8">Equipos Reparados</option>
                 <option value="totalTrainings">Capacitaciones Realizadas</option>
                 <option value="trabajo_realizado">Total de Actividad</option>
