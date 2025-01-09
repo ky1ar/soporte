c80161ec24aca6a2c0397679139e7a54eb04039c
alex <alex@gmail.com>
2025-01-09 14:32:24 -0500

    a1

--- graphics.php
+++ graphics.php
@@ -147,12 +147,6 @@ if (isset($_SESSION['user_id'])) {
                         },
                         options: {
                             responsive: true,
-                            plugins: {
-                                legend: {
-                                    display: true, // Mostrar leyenda
-                                    position: 'top', // Posición de la leyenda ('top', 'bottom', 'left', 'right')
-                                }
-                            },
                             scales: {
                                 y: {
                                     beginAtZero: true
