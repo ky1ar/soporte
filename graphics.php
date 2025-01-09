9eba4262a5807e5ca3f9da6f8cbfc4316c8d72d4
alex <alex@gmail.com>
2025-01-09 16:19:33 -0500

    a1

--- graphics.php
+++ graphics.php
@@ -50,6 +50,7 @@ if (isset($_SESSION['user_id'])) {
 
             <label for="metric_select">Categoría:</label>
             <select id="metric_select" name="metric_select">
+            <option value="">Todos</option>
                 <option value="stat8">Equipos Entregados</option>
                 <option value="totalTrainings">Capacitaciones Finalizadas</option>
                 <option value="trabajo_realizado">Trabajo Realizado</option>
