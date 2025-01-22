4e39fb24f0b44edd906922fdf5720d100479c1b9
alex <alex@gmail.com>
2025-01-22 17:44:39 -0500

    a1

--- training.php
+++ training.php
@@ -202,7 +202,7 @@ if (isset($_SESSION['user_id'])) {
 
                                     </div>
                                     <div class="evi" id="viewEvidencias" data-src="">
-                                        <img src="/assets/img/invoice.svg" alt="Vista previa de la imagen">Sin Archivos
+                                        <img width="12" height="12" src="assets/img/invoice.svg" alt="">Sin Archivos
                                     </div>
                                 </div>
                                 <button type="submit" id="upd_evi">
