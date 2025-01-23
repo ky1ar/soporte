e0bc0baa0e8e5cedd42546e137076cdd7be02bc3
alex <alex@gmail.com>
2025-01-23 18:03:18 -0500

    a1

--- training_report.php
+++ training_report.php
@@ -73,6 +73,7 @@ if (isset($_SESSION['user_id'])) {
                             <button type="submit">Filtrar</button>
                             <button type="button" id="clearFilters">Limpiar Filtros</button>
                         </form>
+                        <p>Resultados: <?php echo isset($result) ? $result->num_rows : 0; ?> fila(s)</p>
                     </div>
 
                     <script>
