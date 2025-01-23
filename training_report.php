fe9c66e3697768d83c53c5ba09456ce06aeb5749
alex <alex@gmail.com>
2025-01-23 18:06:50 -0500

    a1

--- training_report.php
+++ training_report.php
@@ -73,7 +73,7 @@ if (isset($_SESSION['user_id'])) {
                             <button type="submit">Filtrar</button>
                             <button type="button" id="clearFilters">Limpiar Filtros</button>
                         </form>
-                     
+
                     </div>
 
                     <script>
@@ -169,10 +169,11 @@ if (isset($_SESSION['user_id'])) {
                     </table>
                     <span id="totalRows" data-total="<?php echo $result->num_rows; ?>"></span>
                     <div class="pagination" id="pagination"></div>
+                    <p>Resultados: <?php echo isset($result) ? $result->num_rows : 0; ?> fila(s)</p>
+
                 </div>
             </div>
         </div>
-        <p>Resultados: <?php echo isset($result) ? $result->num_rows : 0; ?> fila(s)</p>
     </section>
     <script type="text/javascript" src="assets/js/test.js?v=<?php echo $GLOBALS['ver']; ?>"></script>
 </body>
