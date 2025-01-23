f3d16d2bba588ac1652c941d2a5721c42f39de2e
alex <alex@gmail.com>
2025-01-23 16:43:18 -0500

    a1

--- training_report.php
+++ training_report.php
@@ -1,28 +1,17 @@
-<?php 
+<?php
 session_start();
-
-// Definir el tiempo de inactividad (1 hora en segundos)
-$inactiveTime = 3600; // 3600 segundos = 1 hora
-
-// Verificar si la sesión ya está iniciada
+$inactiveTime = 3600;
 if (isset($_SESSION['user_id'])) {
-    // Verificar si la última actividad está establecida
     if (isset($_SESSION['last_activity'])) {
-        // Comprobar si ha pasado el tiempo de inactividad
         if (time() - $_SESSION['last_activity'] > $inactiveTime) {
-            // La sesión ha estado inactiva durante más de 1 hora, destruirla
-            session_unset();     // Eliminar todas las variables de sesión
-            session_destroy();   // Destruir la sesión
-            // Redirigir al usuario a la página de login
+            session_unset();  
+            session_destroy(); 
             header("Location: krear3dperu");
             exit();
         }
     }
+    $_SESSION['last_activity'] = time();
 
-    // Actualizar la última actividad
-    $_SESSION['last_activity'] = time(); // Actualizar la marca de tiempo de la última actividad
-
-    // Variables de sesión
     $s_id = $_SESSION['user_id'];
     $s_levels = $_SESSION['user_levels'];
     $s_name = $_SESSION['user_name'];
@@ -41,6 +30,7 @@ if (isset($_SESSION['user_id'])) {
 }
 ?>
 </head>
+
 <body class="ky1-adm">
     <?php
     require_once 'includes/bar/topBar_admin.php';
@@ -54,6 +44,9 @@ if (isset($_SESSION['user_id'])) {
                         <h3>Historial de Capacitaciones
                             <p>Listado de usuarios y solicitudes de capacitación</p>
                         </h3>
+                    </div>
+                    <div class="filtros">
+
                     </div>
                     <table aria-describedby="Training Report" class="rpt-tbl" border="0">
                         <tr class="row-hdr">
@@ -91,30 +84,30 @@ if (isset($_SESSION['user_id'])) {
                                 setlocale(LC_TIME, 'es_ES');
                                 $date = strtotime($row['training_date']);
                                 $date = strftime("%e de %B de %Y", $date);
-                                ?>
-                            <tr>
-                                <td><?php echo $row['training_id'] ?></td>
-                                <td class="row-odr">
-                                    <img
-                                    width="48"
-                                    class="tbl-img"
-                                    src="assets/mac/<?php echo $row['machine_slug'] ?>.webp" alt="">
-                                    <div class="tbl-odr">
-                                    <?php echo $row['brand_name']?><span><?php echo $row['machine_model']?></span>
-                                    </div>
-                                </td>
-                                <td><?php echo $row['worker_name'] ? $row['worker_name']:'No aplica' ?></td>
-                                <td><?php echo $row['client_name'] ?></td>
-                                <td><?php echo $date ?></td>
-                                <td><?php echo substr($row['training_start'], 0, -3) ?></td>
-                                <td class="row-tst">
-                                    <span class="st<?php echo $row['state_id'] ?>">
-                                        <?php echo $row['state_name'] ?>
-                                    </span>
-                                </td>
-                                <td class="row-act"><img class="tbl-tec" src="assets/img/dot.svg" alt=""></td>
-                            </tr>
-                            <?php
+                        ?>
+                                <tr>
+                                    <td><?php echo $row['training_id'] ?></td>
+                                    <td class="row-odr">
+                                        <img
+                                            width="48"
+                                            class="tbl-img"
+                                            src="assets/mac/<?php echo $row['machine_slug'] ?>.webp" alt="">
+                                        <div class="tbl-odr">
+                                            <?php echo $row['brand_name'] ?><span><?php echo $row['machine_model'] ?></span>
+                                        </div>
+                                    </td>
+                                    <td><?php echo $row['worker_name'] ? $row['worker_name'] : 'No aplica' ?></td>
+                                    <td><?php echo $row['client_name'] ?></td>
+                                    <td><?php echo $date ?></td>
+                                    <td><?php echo substr($row['training_start'], 0, -3) ?></td>
+                                    <td class="row-tst">
+                                        <span class="st<?php echo $row['state_id'] ?>">
+                                            <?php echo $row['state_name'] ?>
+                                        </span>
+                                    </td>
+                                    <td class="row-act"><img class="tbl-tec" src="assets/img/dot.svg" alt=""></td>
+                                </tr>
+                        <?php
                             endwhile;
                         endif; ?>
                     </table>
@@ -126,4 +119,5 @@ if (isset($_SESSION['user_id'])) {
     </section>
     <script type="text/javascript" src="assets/js/test.js?v=<?php echo $GLOBALS['ver']; ?>"></script>
 </body>
+
 </html>
