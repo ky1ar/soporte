59d5218e4be75a4e89c6a47b6ad774d5c30392ce
alex <alex@gmail.com>
2025-01-27 18:41:34 -0500

    a1

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1114,12 +1114,12 @@ $(document).ready(function () {
               month: "long",
             })
               .format(date)
-              .replace(/\bde\b/g, "de"); // Mantener "de" en minúsculas
+              .replace(/\bde\b/g, "de");
             const capitalizedDate = formattedDate
               .split(" ")
               .map((word, index) => {
                 if (word.toLowerCase() === "de") {
-                  return word; // No capitalizar "de"
+                  return word;
                 }
                 return word.charAt(0).toUpperCase() + word.slice(1);
               })
@@ -1135,13 +1135,11 @@ $(document).ready(function () {
             $("#modalViewCap .viewCap .inf .email").text(data.email);
             $("#modalViewCap .viewCap .comm .pruebas").attr("data-idf", trainingId);
             $("#modalViewCap .viewCap .inf .comprobante").attr("data-idf", trainingId);
-  
             const icon =
               '<img width="12" height="12" src="assets/img/invoice.svg" alt=""> ';
             $("#modalViewCap .viewCap .comm .pruebas").html(
               data.pruebas ? icon + data.pruebas : icon + "Sin Archivos"
             );
-  
             $("#modalViewCap .viewCap .inf a")
               .attr("href", `https://api.whatsapp.com/send?phone=${data.phone}`)
               .text("+" + data.phone);
@@ -1157,7 +1155,6 @@ $(document).ready(function () {
       });
     });
   
-    // Clic en el div .pruebas para cargar el archivo en #mostradorInfoT
     $("#modalViewCap .viewCap .comm .pruebas").click(function () {
       var fileUrl = $(this).text().trim();
       if (fileUrl) {
@@ -1165,42 +1162,40 @@ $(document).ready(function () {
       }
     });
   
-    // Clic en el div .comprobante para cargar el archivo en #mostradorInfoT
     $("#modalViewCap .viewCap .inf .comprobante").click(function () {
-      var fileUrl = $(this).text().trim(); // Ajusta esto según el valor real en data.invoice
+      var fileUrl = $(this).text().trim();
       if (fileUrl) {
         loadFileInViewer(fileUrl);
       }
     });
   
-    // Función para cargar el archivo en el visualizador
     function loadFileInViewer(fileUrl) {
       const extension = fileUrl.split('.').pop().toLowerCase();
       const viewer = $('#mostradorInfoT .visualizador');
-      viewer.empty(); // Limpiar el visualizador
+      viewer.empty();
   
       if (extension === 'pdf') {
-        // Si es un PDF, mostrarlo dentro de un iframe
         viewer.html('<iframe src="' + fileUrl + '" width="100%" height="600px"></iframe>');
       } else if (['jpg', 'jpeg', 'png', 'webp', 'gif'].includes(extension)) {
-        // Si es una imagen, mostrarla
         viewer.html('<img src="' + fileUrl + '" width="100%" alt="Archivo">');
       } else {
         viewer.html('<p>Archivo no compatible.</p>');
       }
-  
-      // Mostrar el visualizador
       $('#mostradorInfoT').addClass('show');
     }
   
     $("#modalViewCap .fondo").click(function () {
       $("#modalViewCap").fadeOut();
     });
-
-    $("#mostradorInfoT .close-btn").click(function () {
-      $('#mostradorInfoT').removeClass('show'); // Ocultar el visualizador
+  
+    // Cerrar el modal de visualización al hacer clic en el fondo de #mostradorInfoT
+    $("#mostradorInfoT").click(function (e) {
+      if ($(e.target).is("#mostradorInfoT")) {
+        $('#mostradorInfoT').removeClass('show');
+      }
     });
   });
+  
   
 
   scheduleSubmit.submit(function (event) {
