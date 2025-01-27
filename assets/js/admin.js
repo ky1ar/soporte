45841b35d2d641e2d7cdd5de74c3d945477cf54a
alex <alex@gmail.com>
2025-01-27 15:22:37 -0500

    a1

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1103,6 +1103,7 @@ $(document).ready(function () {
         url: "routes/getTraining",
         data: { training_id: trainingId },
         success: function (data) {
+          console.log(data);
           $("#modalViewCap .viewCap .comm textarea").val(data.comments);
           $("#modalViewCap").fadeIn();
         },
