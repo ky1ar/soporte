e0471ee32dc4f2b9dae15b9f15854ac4f6ac4d60
alex <alex@gmail.com>
2025-01-27 17:38:23 -0500

    a1

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1135,7 +1135,7 @@ $(document).ready(function () {
             $("#modalViewCap .viewCap .inf .modelo").text(data.model);
             $("#modalViewCap .viewCap .inf .name").text(data.name);
             $("#modalViewCap .viewCap .inf .email").text(data.email);
-            $(".viewCap .comm .pruebas").attr("data-idf", trainingId);
+            $("#modalViewCap .viewCap .comm .pruebas").attr("data-idf", trainingId);
 
             const icon =
               '<img width="12" height="12" src="assets/img/invoice.svg" alt=""> ';
