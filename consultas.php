0ee76c66dd2e735a1a7eb1bbe43da8c2902fc33e
alex <alex@gmail.com>
2025-02-03 11:37:23 -0500

    test

--- consultas.php
+++ consultas.php
@@ -50,10 +50,6 @@ require_once 'includes/common/header.php';
         <div class="wrapper">
             <p class="d">Introduce tu número de documento aquí (DNI / RUC)</p>
             <p class="o">Introduce tu núnero de orden aquí</p>
-            <div>
-                <p>Consultar <span>Orden de Servicio</span></p>
-                <p>Introduce el número de orden y el documento tal cual se muestra en la orden de ingreso proporcionada.</p>
-            </div>
             <form id="searchOrder">
                 <h1>Consultar Orden de Servicio</h1>
                 <p>Introduce el número de orden y el documento tal cual se muestra en la orden de ingreso proporcionada.</p>
