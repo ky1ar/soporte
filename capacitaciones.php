b3f1d7b50d431870d6b8fef8ebab8ffe9c13f1d5
alex <alex@gmail.com>
2025-02-03 10:15:05 -0500

    test

--- capacitaciones.php
+++ capacitaciones.php
@@ -17,7 +17,7 @@ require_once 'includes/common/header.php';
             <!-- <p>Estamos encantados de que inicies tu travesía con nosotros. Prepara tu equipo, lleva a cabo las primeras pruebas y comencemos juntos.</p> -->
         </div>
         <div class="car-prices">
-            <div class="swiffy-slider slider-item-show3 slider-indicators-round slider-indicators-dark slider-indicators-outside slider-indicators-sm">
+            <div class="swiffy-slider slider-item-show3 slider-indicators-dark slider-indicators-outside slider-indicators-highlight slider-indicators-sm">
                 <ul class="slider-container">
                     <li class="slide-visible">
                         <div class="card">
@@ -101,15 +101,7 @@ require_once 'includes/common/header.php';
                 <button type="button" class="slider-nav"></button>
                 <button type="button" class="slider-nav slider-nav-next"></button>
 
-                <ul class="slider-indicators">
-                    <li class="active"></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                
             </div>
         </div>
     </section>
