bd5d7b3cd4e3655a51317608a5f881460d47ab63
alex <alex@gmail.com>
2025-02-04 15:34:53 -0500

    test

--- servicios.php
+++ servicios.php
@@ -17,23 +17,28 @@ require_once 'includes/common/header.php';
             <tr>
                 <th>Descripción</th>
                 <th colspan="2">Costos</th>
+                <th>Criterios de Tamaño</th>
             </tr>
             <tr>
                 <td rowspan="4">Reemplazo de pantalla (precio según modelo)</td>
                 <td>Tamaño</td>
                 <td>Precio</td>
+                <td>Medidas</td>
             </tr>
             <tr>
                 <td>Pequeño</td>
                 <td>S/. 80.00</td>
+                <td>2.8 - 4.3 pulgadas</td>
             </tr>
             <tr>
                 <td>Mediano</td>
                 <td>S/. 100.00</td>
+                <td>5.0 - 6.0 pulgadas</td>
             </tr>
             <tr>
                 <td>Grande</td>
                 <td>S/. 200.00</td>
+                <td>7.0 - 9.0 pulgadas o más</td>
             </tr>
         </table>
     </div>
