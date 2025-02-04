cb09793c07c2f6131a9ca825df7e16d3ee91f9e2
alex <alex@gmail.com>
2025-02-04 15:25:56 -0500

    test

--- servicios.php
+++ servicios.php
@@ -10,8 +10,21 @@ require_once 'includes/common/header.php';
     require_once 'includes/bar/topBar.php';
     require_once 'includes/bar/navigationBar.php';
     ?>
-    <div>
-        hola
+    <div id="sup-servicios">
+        <h1 class="title"></h1>
+        <p class="par-ini"></p>
+        <table>
+            <tr>
+                <th>Descripción</th>
+                <th colspan="2">Costos</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>hola</td>
+                <td>asdsa</td>
+                <td>qwewqe</td>
+            </tr>
+        </table>
     </div>
     <?php require_once 'includes/common/footer.php'; ?>
 </body>
