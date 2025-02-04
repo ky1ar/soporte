7526c8d4a1d3b2261a2a555d45a9fd66daf7046d
alex <alex@gmail.com>
2025-02-04 17:18:36 -0500

    test

--- servicios.php
+++ servicios.php
@@ -2,12 +2,10 @@
 $currentPage = "Servicios";
 require_once 'includes/app/globals.php';
 require_once 'includes/common/header.php';
-require_once 'includes/app/db.php'; // Conectar a la base de datos
+require_once 'includes/app/db.php';
 
 if (isset($_GET['id'])) {
-    $serviceId = $_GET['id']; // Obtener el ID del servicio desde la URL
-
-    // Realizar la consulta con el ID recibido
+    $serviceId = $_GET['id'];
     $sql = "
     SELECT 
         s.id AS servicio_id,
@@ -20,26 +18,19 @@ if (isset($_GET['id'])) {
     FROM Servicios s
     INNER JOIN Servicio srv ON s.id_servicio = srv.id
     WHERE s.id_servicio = ?";
-
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $serviceId);
     $stmt->execute();
     $result = $stmt->get_result();
-
-    // Verificar si hay resultados
     if ($result->num_rows > 0) {
-        // Obtener todas las filas en un array
         $services = $result->fetch_all(MYSQLI_ASSOC);
     } else {
         $errorMessage = "No se encontraron servicios.";
     }
-
-    // Cerrar la consulta y la conexión
     $stmt->close();
     $conn = null;
 }
 ?>
-
 </head>
 
 <body>
@@ -47,14 +38,11 @@ if (isset($_GET['id'])) {
     require_once 'includes/bar/topBar.php';
     require_once 'includes/bar/navigationBar.php';
     ?>
-
     <div id="sup-servicios">
         <?php if (isset($services) && count($services) > 0): ?>
-            <!-- Mostrar el nombre y la introducción del primer servicio, por ejemplo -->
             <h1 class="title"><?php echo $services[0]['nombre_servicio']; ?></h1>
             <p class="intro"><?php echo $services[0]['intro']; ?></p>
         <?php endif; ?>
-
         <table>
             <thead>
                 <tr>
@@ -82,7 +70,6 @@ if (isset($_GET['id'])) {
             </tbody>
         </table>
     </div>
-
     <?php require_once 'includes/common/footer.php'; ?>
 </body>
 
