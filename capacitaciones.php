ffe2ebe32951114778a406de2ba03bc309334bd0
alex <alex@gmail.com>
2025-02-07 17:49:52 -0500

    test

--- capacitaciones.php
+++ capacitaciones.php
@@ -11,10 +11,7 @@ require_once 'includes/common/header.php';
     require_once 'includes/bar/topBar.php';
     require_once 'includes/bar/navigationBar.php';
     ?>
-    <div class="example-cap">
-        <div class="back-ex"></div>
-        <img src="assets/img/example-cap.webp" alt="">
-    </div>
+
     <section id="frontSlider">
         <div class="wrapper">
             <img class="imaCap" width="1920" height="630" src="assets/img/capacitaciones-pc.webp">
@@ -116,7 +113,10 @@ require_once 'includes/common/header.php';
             </div>
         </div>
     </section>
-
+    <div class="example-cap">
+        <div class="back-ex"></div>
+        <img src="assets/img/example-cap.webp" alt="">
+    </div>
     <section id="trainingSection">
         <div class="top">
             <h2>Agenda Aquí</h2>
