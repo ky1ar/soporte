0ac33ff087baabd15f1bacf7973a219189b9d21b
alex <alex@gmail.com>
2025-02-10 12:07:55 -0500

    test

--- includes/common/footer.php
+++ includes/common/footer.php
@@ -70,9 +70,9 @@
     <div class="bottom">
         <div class="wrapper">
             <ul>
-                <li><a href="https://www.tiendakrear3d.com/terminos/condiciones-garantia/">POLÍTICAS Y CONDICIONES</a></li>
-                <li><a href="https://www.tiendakrear3d.com/terminos/garantia-soporte/">POLÍTICAS DE GARANTÍA</a></li>
-                <li><a href="https://www.tiendakrear3d.com/terminos/politicas-de-envios-lima-y-provincias/">POLÍTICAS DE ENVÍOS</a></li>
+                <li><a target="_blank" href="https://www.tiendakrear3d.com/terminos/condiciones-garantia/">POLÍTICAS Y CONDICIONES</a></li>
+                <li><a target="_blank" href="https://www.tiendakrear3d.com/terminos/garantia-soporte/">POLÍTICAS DE GARANTÍA</a></li>
+                <li><a target="_blank" href="https://www.tiendakrear3d.com/terminos/politicas-de-envios-lima-y-provincias/">POLÍTICAS DE ENVÍOS</a></li>
             </ul>
 
             <span class="copyright">
