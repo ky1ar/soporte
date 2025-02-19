f11b049cb26a0ae79a6a381709548625eb8a2a9c
alex <alex@gmail.com>
2025-02-19 16:05:36 -0500

    test

--- stl.php
+++ stl.php
@@ -16,8 +16,8 @@ require_once 'includes/common/header.php';
                 <img src="assets/img/first-banner-top-stls.webp" alt="">
             </div>
             <div class="cont">
-                <h1>Tu primera impresión 3D empieza aquí</h1>
-                <p>Completa el formulario y obtén nuestro <b>pack de STLs GRATIS</b> y dale forma a tus ideas.</p>
+                <h1 class="t1">Tu primera impresión 3D empieza aquí</h1>
+                <p class="p1">Completa el formulario y obtén nuestro <b>pack de STLs GRATIS</b> y dale forma a tus ideas.</p>
                 <form method="POST" action="https://formsubmit.co/sistemas@krear3d.com" enctype="multipart/form-data">
                     <div class="formi">
                         <h1>Rellena este formulario</h1>
