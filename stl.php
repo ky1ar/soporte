9497c501cb074043dc7793268cc7f24de8d99943
alex <alex@gmail.com>
2025-02-20 12:22:02 -0500

    test

--- stl.php
+++ stl.php
@@ -37,49 +37,49 @@ require_once 'includes/common/header.php';
         <div class="swiffy-slider slider-item-show6 slider-nav-round slider-nav-dark slider-nav-sm slider-nav-outside-expand slider-nav-visible slider-nav-autohide slider-indicators-outside">
             <ul class="slider-container">
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-anime.webp" alt="">
                         <p>Animes</p>
                     </div>
                 </li>
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-chibi.webp" alt="">
                         <p>Chibi</p>
                     </div>
                 </li>
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-funkos.webp" alt="">
                         <p>Funkos</p>
                     </div>
                 </li>
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-flex.webp" alt="">
                         <p>Flexibles</p>
                     </div>
                 </li>
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-marvel.webp" alt="">
                         <p>Marvel</p>
                     </div>
                 </li>
                 <li class="slide-visible">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-dc.webp" alt="">
                         <p>DC Comics</p>
                     </div>
                 </li>
                 <li class="">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-sw.webp" alt="">
                         <p>Star Wars</p>
                     </div>
                 </li>
                 <li class="">
-                    <div id="categ">
+                    <div class="categ">
                         <img src="assets/img/cat-stls-sw.webp" alt="">
                         <p>Star Wars</p>
                     </div>
