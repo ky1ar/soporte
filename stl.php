05e122dad24f77b9be0d66a48753491c8f88a86a
alex <alex@gmail.com>
2025-02-20 14:59:39 -0500

    test

--- stl.php
+++ stl.php
@@ -94,6 +94,12 @@ require_once 'includes/common/header.php';
                         <p>Star Wars</p>
                     </div>
                 </li>
+                <li class="slide-visible">
+                    <div class="categ">
+                        <img src="assets/img/cat-stls-sw.webp" alt="">
+                        <p>Star Wars</p>
+                    </div>
+                </li>
             </ul>
 
         </div>
