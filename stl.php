67663edd6062557fc91893547edd3290b2abeb2c
adriannc21 <alexandernajarro17@gmail.com>
2025-02-22 14:36:43 -0500

    test s1

--- stl.php
+++ stl.php
@@ -16,7 +16,7 @@ require_once 'includes/common/header.php';
                 <img src="assets/img/first-banner-top-stls.webp" alt="">
             </div>
             <div class="cont">
-                <h1 class="t1">Tu primera impresión 3D empieza aquí</h1>
+                <h1 class="t1">Tus primera impresión 3D empieza aquí</h1>
                 <p class="p1">Completa el formulario y obtén nuestro<br><b>pack de STLs GRATIS</b> y dale forma a tus ideas.</p>
                 <form method="POST" id="registroFormStls">
                     <div class="formi">
