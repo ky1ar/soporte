38067917720f31809bb69598cd48dbddb978e142
alex <alex@gmail.com>
2025-04-14 15:23:32 -0500

    Consulta de envios

--- pedidos.php
+++ pedidos.php
@@ -14,7 +14,6 @@ require_once 'includes/common/header.php';
     <section id="frontSlider">
         <div class="wrapper">
             <img class="imaSop" width="1920" height="630" src="assets/img/consulta-pc.webp">
-            <!-- <p>Introduce el número de orden y el documento tal cual se muestra en la orden de ingreso proporcionada.</p> -->
         </div>
     </section>
     <h1>hola</h1>
