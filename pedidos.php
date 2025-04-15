8cd2336cba8818dc866f6073eae87ef08a0cd55f
alex <alex@gmail.com>
2025-04-15 12:40:16 -0500

    Consulta de envios

--- pedidos.php
+++ pedidos.php
@@ -25,7 +25,7 @@ require_once 'includes/common/header.php';
 
                 <form action="" method="POST" id="formConsulta">
                     <label for="name">Documento:</label>
-                    <input type="text" name="name" id="documento" placeholder="Ingrese DNI o RUC">
+                    <input type="text" name="name" id="documento" placeholder="Ingrese DNI o RUC" maxlength="11">
                     <button>Consultar</button>
                 </form>
             </div>
