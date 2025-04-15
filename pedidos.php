17df5f3affb44b6562b89f369d0d05ad4d3385e4
alex <alex@gmail.com>
2025-04-15 11:11:09 -0500

    Consulta de envios

--- pedidos.php
+++ pedidos.php
@@ -32,7 +32,7 @@ require_once 'includes/common/header.php';
             <div id="listOrdersShipping">
                 <div class="order">
                     <div class="head">
-                        <p class="order">S232399</p>
+                        <p class="orderNum">S232399</p>
                     </div>
                     <div class="cont">
                         <div class="info">
