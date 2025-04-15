946aa1953d34c01c3d82a785933369585ac1710b
alex <alex@gmail.com>
2025-04-15 12:46:05 -0500

    Consulta de envios

--- assets/js/pedidos.js
+++ assets/js/pedidos.js
@@ -44,16 +44,9 @@
 // });
 
 $(document).ready(function () {
-  // Verificar si ya hay órdenes guardadas en el localStorage
-  const cachedOrders = localStorage.getItem('orders');
-  if (cachedOrders) {
-    showOrders(JSON.parse(cachedOrders)); // Mostrar órdenes guardadas en cache
-  }
-
   $("#formConsulta").on("submit", function (e) {
     e.preventDefault();
     const documento = $("#documento").val().trim();
-    
     $.ajax({
       url: "routes/getOrdersShipping.php",
       type: "POST",
@@ -65,14 +58,27 @@ $(document).ready(function () {
         } else if (response.status === "success") {
           const container = $("#listOrdersShipping");
           container.html('<h1 class="title">Mis Pedidos</h1>'); // Título principal
-          
-          // Guardar las órdenes en localStorage
-          localStorage.setItem('orders', JSON.stringify(response.orders));
-
-          // Mostrar las órdenes
-          showOrders(response.orders);
-
-          // Ocultar el formulario y mostrar las órdenes
+          response.orders.forEach((order) => {
+            container.append(`
+                          <div class="order">
+                              <div class="head">
+                                  <p class="orderNum">Orden: <span>${order.orden}</span></p>
+                                  <p class="agencia">Agencia: <span>${order.nombre_agencia}</span></p>
+                              </div>
+                              <div class="cont">
+                                  <div class="info">
+                                      <p class="status">${order.nombre_status}</p>
+                                      <p class="details">${order.details}</p>
+                                  </div>
+                                  <div class="actions">
+                                      <!-- Botón de Rastrear con los atributos data-code1 y data-code2 -->
+                                      <button class="btn" data-code1="${order.code1}" data-code2="${order.code2}">Rastrear</button>
+                                      <a class="btn" href="https://wa.me/51910900581?text=Hola,%20quisiera%20hacer%20una%20consulta%20sobre%20mi%20compra%20con%20número%20de%20orden%3A%20${order.orden}" target="_blank">Obtener Ayuda</a>
+                                  </div>
+                              </div>
+                          </div>
+                      `);
+          });
           $("#formPedidos").hide();
           container.fadeIn().css("display", "flex");
         }
@@ -82,32 +88,4 @@ $(document).ready(function () {
       },
     });
   });
-
-  // Función para mostrar las órdenes
-  function showOrders(orders) {
-    const container = $("#listOrdersShipping");
-    orders.forEach((order) => {
-      container.append(`
-        <div class="order">
-          <div class="head">
-            <p class="orderNum">Orden: <span>${order.orden}</span></p>
-            <p class="agencia">Agencia: <span>${order.nombre_agencia}</span></p>
-          </div>
-          <div class="cont">
-            <div class="info">
-              <p class="fecha">Llega el 2 de mayo</p>
-              <p class="status">${order.nombre_status}</p>
-              <p class="details">${order.details}</p>
-            </div>
-            <div class="actions">
-              <!-- Botón de Rastrear con los atributos data-code1 y data-code2 -->
-              <button class="btn" data-code1="${order.code1}" data-code2="${order.code2}">Rastrear</button>
-              <a class="btn" href="https://wa.me/51910900581?text=Hola,%20quisiera%20hacer%20una%20consulta%20sobre%20mi%20compra%20con%20número%20de%20orden%3A%20${order.orden}" target="_blank">Obtener Ayuda</a>
-            </div>
-          </div>
-        </div>
-      `);
-    });
-  }
 });
-
