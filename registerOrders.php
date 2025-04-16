169995ff8baf9042f3914f9da1dc766b5cf99743
alex <alex@gmail.com>
2025-04-16 16:52:27 -0500

    Consulta de envios

--- registerOrders.php
+++ registerOrders.php
@@ -15,32 +15,32 @@ require_once 'includes/common/header.php';
         <div id="registerTrackings">
             <h1 class="title">Registro de Tracking</h1>
             <form action="#" method="POST" class="form">
-                <label for="order_number">Order Number:</label>
-                <input type="text" id="order_number" name="order_number" required><br><br>
+                <label for="order_number">N° Orden:</label>
+                <input type="text" id="order_number" name="order_number" required>
 
                 <label for="agency_id">Agency ID:</label>
-                <input type="number" id="agency_id" name="agency_id" required><br><br>
+                <input type="number" id="agency_id" name="agency_id" required>
 
                 <label for="admin_id">Admin ID:</label>
-                <input type="number" id="admin_id" name="admin_id" required><br><br>
+                <input type="number" id="admin_id" name="admin_id" required>
 
                 <label for="code1">Code 1:</label>
-                <input type="text" id="code1" name="code1" required><br><br>
+                <input type="text" id="code1" name="code1" required>
 
                 <label for="code2">Code 2:</label>
-                <input type="text" id="code2" name="code2" required><br><br>
+                <input type="text" id="code2" name="code2" required>
 
                 <label for="client_id">Client ID:</label>
-                <input type="number" id="client_id" name="client_id" required><br><br>
+                <input type="number" id="client_id" name="client_id" required>
 
                 <label for="client_document">Document:</label>
-                <input type="text" id="client_document" name="client[document]" required><br><br>
+                <input type="text" id="client_document" name="client[document]" required>
 
                 <label for="client_name">Name:</label>
-                <input type="text" id="client_name" name="client[name]" required><br><br>
+                <input type="text" id="client_name" name="client[name]" required>
 
                 <label for="client_phone">Phone:</label>
-                <input type="text" id="client_phone" name="client[phone]" required><br><br>
+                <input type="text" id="client_phone" name="client[phone]" required>
 
                 <input type="submit" value="Submit">
             </form>
