0a7d4da4b23ab75d196cf8bca63e1fbfcd4cb6fe
alex <alex@gmail.com>
2025-04-22 17:56:43 -0500

    Register Tracking

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         const documento = $("#documento").val().trim();
 
         if (documento !== '70986545') {
-            $("#error-message").html('<p>Documento incorrecto. Por favor ingrese el DNI correcto.</p>').fadeIn().delay(1500).fadeOut();
+            $("#error-message").html('<p>Documento no encontrado</p>').fadeIn().delay(1500).fadeOut();
             return;
         }
 
