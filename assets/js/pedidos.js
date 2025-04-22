2bf1ead3ce57be8eae9275ba3bcfc01bd7c418e0
adriannc21 <alexandernajarro17@gmail.com>
2025-04-21 23:40:05 -0500

    Tracking

--- assets/js/pedidos.js
+++ assets/js/pedidos.js
@@ -10,35 +10,22 @@ $(document).ready(function () {
     .on("change", function () {
       const val = $(this).val();
       const [ph1, ph2] = placeholders[val] || ["", ""];
-
-      // Definir maxlength solo para Marvisur (3)
-      if (val === "3") {
-        $("#registerTrackings .form .track #code1")
-          .attr("placeholder", ph1)
-          .attr("maxlength", 4);
-      } else {
-        $("#registerTrackings .form .track #code1")
-          .attr("placeholder", ph1)
-          .removeAttr("maxlength");
-      }
-
-      // Definir code2 según agencia
-      let code2Field = "";
-
-      if (val === "2") {
-        // Olva: select
-        code2Field = `
-          <select class="sp" id="code2" name="code2" required>
-            <option value="25">25</option>
-            <option value="24">24</option>
-            <option value="23">23</option>
-            <option value="22">22</option>
-          </select>`;
-      } else {
-        code2Field = `
-          <input type="text" id="code2" name="code2" placeholder="${ph2}" maxlength="15" required>`;
-      }
+      $("#registerTrackings .form .track #code1").attr("placeholder", ph1);
+      const code2Field =
+        val === "2"
+          ? `<select class="sp" id="code2" name="code2" required>
+               <option value="25">25</option>
+               <option value="24">24</option>
+               <option value="23">23</option>
+               <option value="22">22</option>
+             </select>`
+          : `<input type="text" id="code2" name="code2" placeholder="${ph2}" maxlength="15" required>`;
       $("#registerTrackings .form .track #code2").replaceWith(code2Field);
+      if (val === "3") {
+        $("#registerTrackings .form .track #code1").attr("maxlength", "4");
+      } else {
+        $("#registerTrackings .form .track #code1").attr("maxlength", "15");
+      }
     })
     .trigger("change");
 });
